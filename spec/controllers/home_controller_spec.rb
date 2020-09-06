0e45b147aaad569ac09305bd963a8be23b57b64a
DaniilYusha <ilushenko79@gmail.com>
2020-09-06 17:31:22 +0300

    Disable Metrics/BlockLength cop for all specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-
-  describe "GET #index" do
+  describe 'GET #index' do
     before { get :index }
 
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status :success
     end
 
