c7ee6ea85e3f3da5c3c9a296ebaa11962ce8f6f8
DaniilYusha <ilushenko79@gmail.com>
2020-09-11 15:29:42 +0300

    Add open-sans font via yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap-sass")
 require("@fortawesome/fontawesome-free/js/all")
+require('typeface-open-sans')
 require("../stylesheets/application.scss")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
