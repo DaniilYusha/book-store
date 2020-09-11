7d52c13cb862f23adc1c357de9a0b7d2fc476eee
DaniilYusha <ilushenko79@gmail.com>
2020-09-11 15:18:21 +0300

    Require bootstrap, fontawesome and stylesheets in application.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-require("jquery")
-require("bootstrap")
-
-import "@fortawesome/fontawesome-free/js/all";
+require("bootstrap-sass")
+require("@fortawesome/fontawesome-free/js/all")
+require("../stylesheets/application.scss")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
