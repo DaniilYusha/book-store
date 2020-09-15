35b731c8b4d8cad03e95bd8776d7e993d0871103
DaniilYusha <ilushenko79@gmail.com>
2020-09-15 19:45:36 +0300

    Decorate published_at and materials methods for Book model

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -4,4 +4,12 @@ class BookDecorator < ApplicationDecorator
   def authors
     object.authors.map(&:name).join ', '
   end
+
+  def published_at
+    object.published_at.year
+  end
+
+  def materials
+    object.materials.map(&:name).join(', ').capitalize
+  end
 end
