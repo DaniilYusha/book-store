d7a6e5f434deb78af3221e8c6296acb2787bd1cd
DaniilYusha <ilushenko79@gmail.com>
2020-09-15 12:55:36 +0300

    Add price field and uniqueness for title field to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,6 @@ class Book < ApplicationRecord
   has_many :author_book
   has_many :authors, through: :author_book
 
-  validates :title, presence: true, length: { maximum: TITLE_MAX_LENGTH }
+  validates :title, presence: true, uniqueness: true, length: { maximum: TITLE_MAX_LENGTH }
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
