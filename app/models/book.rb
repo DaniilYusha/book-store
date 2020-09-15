569849a4abe2eabde55eb6d89dc9bdf0caf6fada
DaniilYusha <ilushenko79@gmail.com>
2020-09-15 19:42:34 +0300

    Add relation with materials and validation for published_at field in Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,12 @@ class Book < ApplicationRecord
 
   has_many :author_book
   has_many :authors, through: :author_book
+  has_many :book_material
+  has_many :materials, through: :book_material
+  
+  belongs_to :category
 
   validates :title, presence: true, uniqueness: true, length: { maximum: TITLE_MAX_LENGTH }
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+  validates :published_at, presence: true
 end
