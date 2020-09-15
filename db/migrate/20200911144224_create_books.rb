fa50b7ec6c94ec8e3dad65f513aeee73929027a0
DaniilYusha <ilushenko79@gmail.com>
2020-09-15 12:55:36 +0300

    Add price field and uniqueness for title field to Book

--- db/migrate/20200911144224_create_books.rb
+++ db/migrate/20200911144224_create_books.rb
@@ -1,8 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
-      t.string :title, null: false
+      t.string :title, unique: true, null: false
       t.text :description, default: ''
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
