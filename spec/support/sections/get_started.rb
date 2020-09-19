93f438fe13a364c1c289c6ea9e94ec3ac26396c3
DaniilYusha <ilushenko79@gmail.com>
2020-09-19 19:57:10 +0300

    Add Sections module for sections

--- spec/support/sections/get_started.rb
+++ spec/support/sections/get_started.rb
@@ -1,5 +1,7 @@
-class GetStarted < SitePrism::Section
-  element :get_started_button, 'button.btn-default'
-  element :greeting, 'h2.h1'
-  element :introduction, 'div.mb-res-50>p'
+module Sections
+  class GetStarted < SitePrism::Section
+    element :get_started_button, 'button.btn-default'
+    element :greeting, 'h2.h1'
+    element :introduction, 'div.mb-res-50>p'
+  end
 end
--- spec/support/sections/slider.rb
+++ spec/support/sections/slider.rb
@@ -1,3 +1,5 @@
-class Slider < SitePrism::Section
-  element :buy_now_button, 'div.general-align>button.btn-primary'
+module Sections
+  class Slider < SitePrism::Section
+    element :buy_now_button, 'div.general-align>button.btn-primary'
+  end
 end
