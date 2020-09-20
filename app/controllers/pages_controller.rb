383f76b65169ce9c4f5b82a5a71f11318f40f034
DaniilYusha <ilushenko79@gmail.com>
2020-09-20 13:20:53 +0300

    Add latest_books instance variable for PagesController index method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
+  LAST_ADDED_BOOKS_COUNT = 3
+
   def home
+    @latest_books = BookDecorator.decorate_collection Book.includes(:authors).last(LAST_ADDED_BOOKS_COUNT)
+    
     render 'pages/home/index'
   end
 end
