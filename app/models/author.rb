e611c36ac7d23438dfb28041e8ca50fc57a87137
DaniilYusha <ilushenko79@gmail.com>
2020-09-22 14:58:29 +0300

    Add format validation for Author first_name and last_name fields

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,11 @@
 class Author < ApplicationRecord
   NAME_MAX_LENGTH = 30
+  NAMES_FORMAT_PATTERN = /[a-zA-z]/.freeze
 
-  has_many :author_book
-  has_many :books, through: :author_book
+  has_many :author_book, dependent: :destroy
+  has_many :books, through: :author_book, dependent: :destroy
 
   validates :first_name, :last_name, presence: true,
-                                     length: { maximum: NAME_MAX_LENGTH }
+                                     length: { maximum: NAME_MAX_LENGTH },
+                                     format: { with: NAMES_FORMAT_PATTERN }
 end
