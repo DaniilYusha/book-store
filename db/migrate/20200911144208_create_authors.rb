913071f407d5f82cbab443813d72e1267e7de77b
DaniilYusha <ilushenko79@gmail.com>
2020-09-22 14:21:28 +0300

    Add first_name and last_name fields to authors table

--- db/migrate/20200911144208_create_authors.rb
+++ db/migrate/20200911144208_create_authors.rb
@@ -1,7 +1,8 @@
 class CreateAuthors < ActiveRecord::Migration[6.0]
   def change
     create_table :authors do |t|
-      t.string :name, unique: true, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
 
       t.timestamps
     end
