bc95333cb493407ff750925176af3754009c6229
DaniilYusha <ilushenko79@gmail.com>
2020-09-22 14:19:56 +0300

    Add height, width and depth fields to books table

--- db/migrate/20200911144224_create_books.rb
+++ db/migrate/20200911144224_create_books.rb
@@ -4,6 +4,9 @@ class CreateBooks < ActiveRecord::Migration[6.0]
       t.string :title, unique: true, null: false
       t.text :description, default: ''
       t.decimal :price, precision: 8, scale: 2, null: false
+      t.decimal :height, precision: 6, scale: 2, null: false
+      t.decimal :width, precision: 6, scale: 2, null: false
+      t.decimal :depth, precision: 6, scale: 2, null: false
       t.date :published_at, null: false
       t.belongs_to :category, foreign_key: true, index: true
 
