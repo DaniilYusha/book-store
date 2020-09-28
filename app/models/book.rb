689a3f6dc793e73b50b8ab84d2f7d71829bc1eae
DaniilYusha <ilushenko79@gmail.com>
2020-09-28 15:08:53 +0300

    Add with_authors scope to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,8 @@ class Book < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true, length: { maximum: TITLE_MAX_LENGTH }
   validates :price, presence: true,
-                    format: { with: /\A\d+(?:\.\d{2})?\z/ },
                     numericality: { greater_than: 0, less_than: 1000000 }
   validates :published_at, presence: true
+
+  scope :with_authors, -> { includes [:authors] }
 end
