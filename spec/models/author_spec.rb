cd3df73ccf0b45961f13a7cd28a095552340fcd1
DaniilYusha <ilushenko79@gmail.com>
2020-09-30 23:40:55 +0300

    Write specs for Author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
-
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:author) }
+
+  describe 'associations' do
+    it { should have_many(:author_book).dependent(:destroy) }
+    it { should have_many(:books).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_length_of(:first_name).is_at_most(described_class::NAME_MAX_LENGTH) }
+    it { should validate_length_of(:last_name).is_at_most(described_class::NAME_MAX_LENGTH) }
+  end
 end
