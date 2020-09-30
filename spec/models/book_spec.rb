9eab036fe757f25fb8cc0137e6acce399c8486ea
DaniilYusha <ilushenko79@gmail.com>
2020-09-30 23:41:36 +0300

    Write specs for Book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,20 @@
-require 'rails_helper'
-
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:book) }
+
+  describe 'associations' do
+    it { should have_many(:author_book).dependent(:destroy) }
+    it { should have_many(:authors).dependent(:destroy) }
+    it { should have_many(:book_material) }
+    it { should have_many(:materials) }
+    it { should belong_to(:category) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(described_class::TITLE_MAX_LENGTH) }
+    it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price).is_greater_than(described_class::MIN_PRICE) }
+    it { should validate_numericality_of(:price).is_less_than(described_class::MAX_PRICE) }
+    it { should validate_presence_of(:published_at) }
+  end
 end
