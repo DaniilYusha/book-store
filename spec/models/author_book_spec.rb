1f45fcb1e422e125a3a2bf1938d508d02f3cdb92
DaniilYusha <ilushenko79@gmail.com>
2020-09-30 23:40:37 +0300

    Write specs for AuthorBook model

--- spec/models/author_book_spec.rb
+++ spec/models/author_book_spec.rb
@@ -1,5 +1,6 @@
-require 'rails_helper'
-
 RSpec.describe AuthorBook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:author) }
+    it { should belong_to(:book) }
+  end
 end
