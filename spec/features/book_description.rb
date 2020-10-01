2c496e212a5b86373f4bda2930b0c8e2371b601c
DaniilYusha <ilushenko79@gmail.com>
2020-10-01 20:07:39 +0300

    Add specs for book_description section

---
+++ spec/features/book_description.rb
@@ -0,0 +1,42 @@
+RSpec.describe 'BookDescription', type: :feature do
+  let(:book) { create(:book_with_associations).decorate }
+  let(:book_page) { Pages::Book.new }
+
+  before { visit book_path(book) }
+
+  it 'has current path' do
+    expect(book_page).to have_current_path book_path(book)
+  end
+
+  it 'has book title' do
+    expect(book_page.book_descriptin).to have_book_title
+  end
+
+  it 'has book authors' do
+    expect(book_page.book_descriptin).to have_book_authors
+  end
+
+  it 'has book price' do
+    expect(book_page.book_descriptin).to have_book_price
+  end
+
+  it 'has add to cart button' do
+    expect(book_page.book_descriptin).to have_add_to_cart_button
+  end
+
+  it 'has book description' do
+    expect(book_page.book_descriptin).to have_book_description
+  end
+
+  it 'has book description' do
+    expect(book_page.book_descriptin).to have_publication_year
+  end
+
+  it 'has book dimensions' do
+    expect(book_page.book_descriptin).to have_dimensions
+  end
+
+  it 'has book materials' do
+    expect(book_page.book_descriptin).to have_materials
+  end
+end
