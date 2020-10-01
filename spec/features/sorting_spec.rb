1b2c0444c8249d41c3714e8d2f65666516f8c210
DaniilYusha <ilushenko79@gmail.com>
2020-10-01 20:08:54 +0300

    Add specs for sorting section

---
+++ spec/features/sorting_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe 'Sorting', type: :feature do
+  let(:categories) { create_list(:category, PagesController::LAST_ADDED_BOOKS_COUNT) }
+  let(:catalog_page) { Pages::Catalog.new }
+
+  before do
+    visit books_path
+  end
+
+  it 'has categories links' do
+    expect(catalog_page.sorting).to have_categories_links(
+      count: PagesController::LAST_ADDED_BOOKS_COUNT
+    )
+  end
+
+  it 'has sorting links' do
+    expect(catalog_page.sorting).to have_sorting_links
+  end
+end
