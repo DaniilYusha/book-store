083f832360683cb9bba67def979ca83a6e603cea
DaniilYusha <ilushenko79@gmail.com>
2020-10-01 11:24:13 +0300

    Add name field validation in Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
   has_many :books, dependent: :destroy
 
+  validates :name, presence: true, uniqueness: true
+
   scope :with_books, -> { includes [:books] }
 end
