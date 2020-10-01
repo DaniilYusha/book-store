d599d4718b3cfac810668b4839fe4f665175c313
DaniilYusha <ilushenko79@gmail.com>
2020-10-01 13:09:19 +0300

    Add tests for assigned instance variables in BooksController

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,35 +1,59 @@
 RSpec.describe BooksController, type: :controller do
-  let(:book) { create(:book_with_associations).decorate }
-
   describe 'GET /books' do
-    before { visit books_path }
+    it 'assigns @books_count' do
+      create_list(:book, 3)
+      get :index
+      expect(assigns(:books_count)).to eq Book.count
+    end
 
-    it 'has current path' do
-      expect(page).to have_current_path books_path
+    it 'assings @books' do
+      categories = create_list(:category, 3)
+      get :index
+      expect(assigns(:books)).to match_array(SortedBooksQuery.new(categories).all.decorate)
+    end
+
+    it 'assigns @current_category' do
+      get :index, params: { category_id: 1 }
+      expect(assigns(:current_category)).to eq '1'
     end
 
     it 'returns http success' do
+      get :index
       expect(response).to have_http_status :success
     end
 
     it 'render books/index template' do
+      get :index
       expect(response).to render_template :index
+    end
+
+    it 'has current path' do
+      visit books_path
+      expect(page).to have_current_path books_path
     end
   end
 
   describe 'GET /books/{id}' do
-    before { visit book_path(book) }
+    let(:book) { create(:book_with_associations).decorate }
 
-    it 'has current path' do
-      expect(page).to have_current_path book_path(book)
+    it 'assigns @book' do
+      get :show, params: { id: book.id }
+      expect(assigns(:book)).to eq Book.includes(:authors).find(book.id).decorate
     end
 
     it 'returns http success' do
+      get :show, params: { id: book.id }
       expect(response).to have_http_status :success
     end
 
     it 'render books/show template' do
+      get :show, params: { id: book.id }
       expect(response).to render_template :show
+    end
+
+    it 'has current path' do
+      visit book_path(book)
+      expect(page).to have_current_path book_path(book)
     end
   end
 end
