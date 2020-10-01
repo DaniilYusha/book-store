b28e257c456e70b145681b17bb8d29498ae0f9a1
DaniilYusha <ilushenko79@gmail.com>
2020-10-01 11:28:09 +0300

    Add name field validation test in Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,12 @@
 RSpec.describe Category, type: :model do
+  subject { create(:category) }
+
   describe 'associations' do
     it { should have_many(:books).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 end
