7569b9eb8260af7adab12805796db77602b6911c
DaniilYusha <ilushenko79@gmail.com>
2020-10-11 17:14:03 +0300

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  PASSWORD_FORMAT = /\A
+    (?!.*\s)
+    (?=.*\d)
+    (?=.*[a-z])
+    (?=.*[A-Z])
+  /x
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+
+  validates :password, format: { with: PASSWORD_FORMAT }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.uid = auth.uid
+      user.provider = auth.provider
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+end
