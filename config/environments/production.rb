0ef5c1b330c51cbbff631c61f5091d51ecf83316
DaniilYusha <ilushenko79@gmail.com>
2020-10-11 17:21:48 +0300

    Add mailing configuration for each environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.google.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
+  config.action_mailer.default_url_options = { host: 'book-store-rails.herokuapp.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
