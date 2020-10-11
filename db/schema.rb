dddf8cc90e4250db6f1b7541609bfd821b323f7a
DaniilYusha <ilushenko79@gmail.com>
2020-10-11 17:13:40 +0300

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -12,57 +12,57 @@
 
 ActiveRecord::Schema.define(version: 20_201_006_092_129) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'author_books', force: :cascade do |t|
-    t.bigint 'author_id'
-    t.bigint 'book_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['author_id'], name: 'index_author_books_on_author_id'
-    t.index ['book_id'], name: 'index_author_books_on_book_id'
+  create_table "author_books", force: :cascade do |t|
+    t.bigint "author_id"
+    t.bigint "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_author_books_on_author_id"
+    t.index ["book_id"], name: "index_author_books_on_book_id"
   end
 
-  create_table 'authors', force: :cascade do |t|
-    t.string 'first_name', null: false
-    t.string 'last_name', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'book_materials', force: :cascade do |t|
-    t.bigint 'book_id'
-    t.bigint 'material_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['book_id'], name: 'index_book_materials_on_book_id'
-    t.index ['material_id'], name: 'index_book_materials_on_material_id'
+  create_table "book_materials", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "material_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_materials_on_book_id"
+    t.index ["material_id"], name: "index_book_materials_on_material_id"
   end
 
-  create_table 'books', force: :cascade do |t|
-    t.string 'title', null: false
-    t.text 'description', default: ''
-    t.decimal 'price', precision: 8, scale: 2, null: false
-    t.decimal 'height', precision: 6, scale: 2, null: false
-    t.decimal 'width', precision: 6, scale: 2, null: false
-    t.decimal 'depth', precision: 6, scale: 2, null: false
-    t.date 'published_at', null: false
-    t.bigint 'category_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['category_id'], name: 'index_books_on_category_id'
+  create_table "books", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", default: ""
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.decimal "height", precision: 6, scale: 2, null: false
+    t.decimal "width", precision: 6, scale: 2, null: false
+    t.decimal "depth", precision: 6, scale: 2, null: false
+    t.date "published_at", null: false
+    t.bigint "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_books_on_category_id"
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'materials', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "materials", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table 'users', force: :cascade do |t|
