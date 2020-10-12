3c6711ff5b65c6f7959c96e729e253e6ac9a7c32
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 13:40:15 +0300

    Add some links to header section

--- spec/support/containers/header.rb
+++ spec/support/containers/header.rb
@@ -5,7 +5,9 @@ module Sections
     element :my_account_link, 'div.hidden-xs>ul>li>a.my_account_link'
     element :shop_link, 'li.dropdown>a.shop_link'
     elements :categories_links, "ul.dropdown-menu li:nth-child(-n+#{PagesController::LAST_ADDED_BOOKS_COUNT})"
-    element :log_out_link, '.dropdown-menu>li>a.log_out_link'
+    element :log_out_link, '.log_out_link'
+    element :log_in_link, '.log_in_link'
+    element :sign_up_link, '.sign_up_link'
 
     def click_home_link
       home_link.click
