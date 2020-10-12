f193502516263e23c06b03782717d7197a0cd3f8
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 13:41:49 +0300

    Add footer specs for logged in user

--- spec/features/footer_spec.rb
+++ spec/features/footer_spec.rb
@@ -15,14 +15,6 @@ RSpec.describe 'Footer', type: :feature do
     expect(home_page.footer).to have_shop_link
   end
 
-  it "has `#{I18n.t 'links.orders'}` link" do
-    expect(home_page.footer).to have_orders_link
-  end
-
-  it "has `#{I18n.t 'links.settings'}` link" do
-    expect(home_page.footer).to have_settings_link
-  end
-
   it 'has email' do
     expect(home_page.footer).to have_email
   end
@@ -43,5 +35,23 @@ RSpec.describe 'Footer', type: :feature do
   it "click `#{I18n.t 'links.home'}` link" do
     home_page.footer.click_home_link
     expect(home_page).to be_displayed
+  end
+
+  context 'when user signed in' do
+    let(:user) { create(:user) }
+    let(:sign_in_page) { Pages::SignIn.new }
+
+    before do
+      visit new_user_session_path
+      sign_in_page.sign_in_form.authenticate_user user.email, user.password
+    end
+
+    it "has `#{I18n.t 'links.orders'}` link" do
+      expect(home_page.footer).to have_orders_link
+    end
+
+    it "has `#{I18n.t 'links.settings'}` link" do
+      expect(home_page.footer).to have_settings_link
+    end
   end
 end
