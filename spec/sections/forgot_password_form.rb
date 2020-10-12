bfbb582f98f244a755f566b9389940ed3790b81b
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 13:43:07 +0300

    Add forgot_password_form for forgot_password page

--- spec/sections/forgot_password_form.rb
+++ spec/sections/forgot_password_form.rb
@@ -1,9 +1,9 @@
 module Sections
   class ForgotPasswordForm < SitePrism::Section
     element :email_label, 'label[for="user_email"]'
-    element :email_field, '#user_email'
+    element :email_field, '#email'
     element :send_instructions_button, 'input[type="submit"]'
-    element :cancel_button, 'button.reset-btn'
+    element :cancel_button, 'button[type="button"]'
 
     def send_forgot_password_instructions(email)
       email_field.set email
