1c6b5c89468c96f58a131457057b4c920b291db6
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 16:25:03 +0300

    Fix rubocop offenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,85 +10,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20_201_006_092_129) do
-=======
-ActiveRecord::Schema.define(version: 2020_10_06_092129) do
-
->>>>>>> 05c3ebf... Add users table
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "author_books", force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "book_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_author_books_on_author_id"
-    t.index ["book_id"], name: "index_author_books_on_book_id"
+  create_table 'author_books', force: :cascade do |t|
+    t.bigint 'author_id'
+    t.bigint 'book_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['author_id'], name: 'index_author_books_on_author_id'
+    t.index ['book_id'], name: 'index_author_books_on_book_id'
   end
 
-  create_table "authors", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'authors', force: :cascade do |t|
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "book_materials", force: :cascade do |t|
-    t.bigint "book_id"
-    t.bigint "material_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["book_id"], name: "index_book_materials_on_book_id"
-    t.index ["material_id"], name: "index_book_materials_on_material_id"
+  create_table 'book_materials', force: :cascade do |t|
+    t.bigint 'book_id'
+    t.bigint 'material_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['book_id'], name: 'index_book_materials_on_book_id'
+    t.index ['material_id'], name: 'index_book_materials_on_material_id'
   end
 
-  create_table "books", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "description", default: ""
-    t.decimal "price", precision: 8, scale: 2, null: false
-    t.decimal "height", precision: 6, scale: 2, null: false
-    t.decimal "width", precision: 6, scale: 2, null: false
-    t.decimal "depth", precision: 6, scale: 2, null: false
-    t.date "published_at", null: false
-    t.bigint "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_books_on_category_id"
+  create_table 'books', force: :cascade do |t|
+    t.string 'title', null: false
+    t.text 'description', default: ''
+    t.decimal 'price', precision: 8, scale: 2, null: false
+    t.decimal 'height', precision: 6, scale: 2, null: false
+    t.decimal 'width', precision: 6, scale: 2, null: false
+    t.decimal 'depth', precision: 6, scale: 2, null: false
+    t.date 'published_at', null: false
+    t.bigint 'category_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['category_id'], name: 'index_books_on_category_id'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "materials", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'materials', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "provider"
-    t.string "uid"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'provider'
+    t.string 'uid'
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
@@ -109,11 +105,14 @@ ActiveRecord::Schema.define(version: 2020_10_06_092129) do
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
+=======
+>>>>>>> 2f8cc24... Fix rubocop offenses
   add_foreign_key 'author_books', 'authors'
   add_foreign_key 'author_books', 'books'
   add_foreign_key 'book_materials', 'books'
   add_foreign_key 'book_materials', 'materials'
   add_foreign_key 'books', 'categories'
+<<<<<<< HEAD
 =======
   add_foreign_key "author_books", "authors"
   add_foreign_key "author_books", "books"
@@ -121,4 +120,6 @@ ActiveRecord::Schema.define(version: 2020_10_06_092129) do
   add_foreign_key "book_materials", "materials"
   add_foreign_key "books", "categories"
 >>>>>>> 05c3ebf... Add users table
+=======
+>>>>>>> 2f8cc24... Fix rubocop offenses
 end
