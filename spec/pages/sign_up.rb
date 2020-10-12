31f2ad8d67277f653e49c4f18d39d5e205b3787b
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 12:34:36 +0300

    Remove comments from sign_up_form section and sign_up page

--- spec/pages/sign_up.rb
+++ spec/pages/sign_up.rb
@@ -5,9 +5,5 @@ module Pages
     section :auth_form_header, Sections::AuthFormHeader, '.container>div.text-center'
     section :sign_up_form, Sections::SignUpForm, '.general-form'
     sections :links, Sections::SignUpLinks, '.general-sign-text'
-
-    # def form_links
-    #   links.map { |link| link.title['href'] }
-    # end
   end
 end
