ce52faa6c4604c0a95051a41c37bdae00858b47a
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 15:54:42 +0300

    Add configurations for omniauth and devise in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
+OmniAuth.config.test_mode = true
+
 Dir[Rails.root.join('spec', 'config', '**', '*.rb')].sort.each { |file| require file }
 Dir[Rails.root.join('spec', 'sections', '**', '*.rb')].sort.each { |file| require file }
 Dir[Rails.root.join('spec', 'pages', '**', '*.rb')].sort.each { |file| require file }
@@ -20,4 +22,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
