de686c41f15bb34d4409c940a1698cd22c2dfae3
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 16:25:03 +0300

    Fix rubocop offenses

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
       @user = User.from_omniauth(request.env['omniauth.auth'])
-
       @user.persisted? ? authenticate_user : move_to_registration
     end
 
