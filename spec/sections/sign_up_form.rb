d88816b5e22b169118502d02f3ab82dd32b4516a
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 11:59:59 +0300

    Add sections for sign up page

--- spec/sections/sign_up_form.rb
+++ spec/sections/sign_up_form.rb
@@ -1,11 +1,13 @@
 module Sections
   class SignUpForm < SitePrism::Section
     element :email_label, 'label[for="user_email"]'
-    element :email_field, '#user_email'
+    element :email_field, '#email'
     element :password_label, 'label[for="user_password"]'
-    element :password_field, '#user_password'
+    element :password_field, '#password'
+    # element :forgot_password_link, 'a.help-block'
+    # element :remember_me_checkbox, '.checkbox-label'
     element :confirm_password_label, 'label[for="user_password_confirmation"]'
-    element :confirm_password_field, '#user_password_confirmation'
+    element :confirm_password_field, '#confirm'
     element :sign_up_button, 'input[type="submit"]'
 
     def registrate_user(email, password, password_confirmation)
