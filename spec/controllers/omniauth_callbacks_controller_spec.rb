533f0089fb57d1c48163f2b0cf09650c1887b38e
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 15:55:27 +0300

    Add specs for OmniauthCallbacksController

---
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe Users::OmniauthCallbacksController, type: :controller do
+  let(:facebook_hash) { OmniAuth::AuthHash.new(Faker::Omniauth.facebook) }
+
+  before do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    request.env["omniauth.auth"] = facebook_hash
+  end
+
+  it 'has regirect status' do
+    get :facebook
+    expect(response).to have_http_status :redirect
+  end
+
+  context 'when user exist' do
+    before do
+      allow(User).to receive(:from_omniauth).and_return(create(:user))
+      get :facebook
+    end
+
+    it { expect(response).to redirect_to('/') }  
+  end
+end
