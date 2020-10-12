f31230ce6fdf0d84d5a7a1b72a2c4d4431dc7142
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 11:59:59 +0300

    Add sections for sign up page

--- spec/sections/sign_up_form.rb
+++ spec/sections/sign_up_form.rb
@@ -4,6 +4,8 @@ module Sections
     element :email_field, '#email'
     element :password_label, 'label[for="user_password"]'
     element :password_field, '#password'
+    # element :forgot_password_link, 'a.help-block'
+    # element :remember_me_checkbox, '.checkbox-label'
     element :confirm_password_label, 'label[for="user_password_confirmation"]'
     element :confirm_password_field, '#confirm'
     element :sign_up_button, 'input[type="submit"]'
