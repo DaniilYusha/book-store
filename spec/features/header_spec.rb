efb764fc9d35597d3f1c4e56b786a7ce21038c7a
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 13:42:08 +0300

    Add header specs for logged in user

--- spec/features/header_spec.rb
+++ spec/features/header_spec.rb
@@ -23,12 +23,34 @@ RSpec.describe 'Header', type: :feature do
     expect(home_page.header).to have_shop_link
   end
 
-  it 'has log_out link' do
-    expect(home_page.header).to have_log_out_link
+  it 'has log_in link' do
+    expect(home_page.header).to have_log_in_link
+  end
+
+  it 'has sign_up link' do
+    expect(home_page.header).to have_sign_up_link
   end
 
   it 'click home link' do
     home_page.header.click_home_link
     expect(home_page).to be_displayed
+  end
+
+  context 'when user signed in' do
+    let(:user) { create(:user) }
+    let(:sign_in_page) { Pages::SignIn.new }
+
+    before do
+      visit new_user_session_path
+      sign_in_page.sign_in_form.authenticate_user user.email, user.password
+    end
+
+    it 'has my_account link' do
+      expect(home_page.header).to have_my_account_link
+    end
+
+    it 'has log_out link' do
+      expect(home_page.header).to have_log_out_link
+    end
   end
 end
