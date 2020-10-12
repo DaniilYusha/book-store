a56a3e6a0b49f5d645e7c7b926a099841f83bdee
DaniilYusha <ilushenko79@gmail.com>
2020-10-12 12:00:27 +0300

    Add sign up page

--- spec/pages/sign_up.rb
+++ spec/pages/sign_up.rb
@@ -1,9 +1,13 @@
 module Pages
   class SignUp < SitePrism::Page
-    set_url '/users/sign_up'
+    set_url '/sign_up'
 
     section :auth_form_header, Sections::AuthFormHeader, '.container>div.text-center'
     section :sign_up_form, Sections::SignUpForm, '.general-form'
     sections :links, Sections::SignUpLinks, '.general-sign-text'
+
+    # def form_links
+    #   links.map { |link| link.title['href'] }
+    # end
   end
 end
