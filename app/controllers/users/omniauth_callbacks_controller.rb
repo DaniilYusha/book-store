4e7c2aae8565db071c999ddd9b03c513897a4487
DaniilYusha <ilushenko79@gmail.com>
2020-10-13 14:50:49 +0300

    Fix rubocop offenses

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ module Users
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, :kind => 'Facebook') if is_navigational_format?
+        set_flash_message(:notice, :success, kind: I18n.t(:provider)) if is_navigational_format?
       else
         session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
         redirect_to new_user_registration_url
