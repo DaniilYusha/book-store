6910d730a7a7f83d214e596cfb004a4cd21c6ded
DaniilYusha <ilushenko79@gmail.com>
2020-10-13 13:37:12 +0300

    Change facebook login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,13 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+      @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
-        sign_in_and_redirect @user, :event => :authentication
-        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, :kind => 'Facebook') if is_navigational_format?
       else
-        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
         redirect_to new_user_registration_url
       end
     end
@@ -15,17 +15,5 @@ module Users
     def failure
       redirect_to root_path
     end
-
-    private
-
-    # def authenticate_user
-    #   sign_in_and_redirect @user, event: :authentication
-    #   set_flash_message(:notice, :success, kind: I18n.t(:provider)) if is_navigational_format?
-    # end
-    #
-    # def move_to_registration
-    #   session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
-    #   redirect_to new_user_registration_url
-    # end
   end
 end
