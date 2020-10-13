de3494eab02a2967e025ae266d90f8e4e4ef4d6c
DaniilYusha <ilushenko79@gmail.com>
2020-10-13 12:31:25 +0300

    Change facebook login logic

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
       @user = User.from_omniauth(request.env['omniauth.auth'])
-      @user.persisted? ? authenticate_user : move_to_registration
+      authenticate_user
     end
 
     def failure
