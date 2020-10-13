3114e02bf82a75acc81173567fd258863c4abfa8
DaniilYusha <ilushenko79@gmail.com>
2020-10-13 15:03:51 +0300

    Split facebook method in OmniauthCallbacksController

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,5 +9,17 @@ module Users
     def failure
       redirect_to root_path
     end
+
+    private
+
+    def authenticate_user
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: I18n.t(:provider)) if is_navigational_format?
+    end
+
+    def move_to_registration
+      session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_url
+    end
   end
 end
