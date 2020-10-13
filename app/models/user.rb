2cff0de92324b151b57196ff01ae512cd2b0c202
DaniilYusha <ilushenko79@gmail.com>
2020-10-13 13:02:44 +0300

    Add some changes to facebook login logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,8 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.uid = auth.uid
-      user.provider = auth.provider
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
+      user.password = Devise.friendly_token[0,20]
     end
   end
 end
