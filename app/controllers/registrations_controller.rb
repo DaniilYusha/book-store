2cc3a67c5a187e4ba84114e0f96ba9f72f24e3b9
DaniilYusha <ilushenko79@gmail.com>
2020-10-23 19:27:36 +0300

    Override devise RegistrationController

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!
+
+  def edit
+    set_forms
+  end
+
+  def update_resource(resource, params)
+    without_password? ? resource.update_without_password(params) : super(resource, params)
+  end
+
+  private
+
+  def set_forms
+    @billing_form = AddressForm.new current_user.billing_address
+    @shipping_form = AddressForm.new current_user.shipping_address
+  end
+
+  def without_password?
+    params[resource_name][:password].blank? && params[resource_name].key?(:email)
+  end
+end
