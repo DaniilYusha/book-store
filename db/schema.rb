002696edb3ef148ef805928de30f984f505a21c6
DaniilYusha <ilushenko79@gmail.com>
2020-10-23 19:15:08 +0300

    Create addresses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_006_092_129) do
+ActiveRecord::Schema.define(version: 20_201_017_072_200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'addresses', force: :cascade do |t|
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.string 'address', null: false
+    t.string 'city', null: false
+    t.string 'zip_code', null: false
+    t.string 'country', null: false
+    t.string 'phone', null: false
+    t.integer 'address_type', null: false
+    t.string 'addressable_type'
+    t.bigint 'addressable_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[addressable_type addressable_id], name: 'index_addresses_on_addressable_type_and_addressable_id'
+  end
 
   create_table 'author_books', force: :cascade do |t|
     t.bigint 'author_id'
