ebc973dd63621ea742ff42b8f402eb597e84330f
DaniilYusha <ilushenko79@gmail.com>
2020-10-23 19:17:19 +0300

    Add relations to User model with Address model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
   PASSWORD_FORMAT = /\A(?!.*\s)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/x.freeze
 
+  has_many :addresses, as: :addressable, dependent: :destroy
+  has_one :billing_address, -> { where(address_type: 0) }, class_name: 'Address',
+                                                           as: :addressable,
+                                                           dependent: :destroy
+  has_one :shipping_address, -> { where(address_type: 1) }, class_name: 'Address',
+                                                            as: :addressable,
+                                                            dependent: :destroy
+
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
 
-  validates :password, format: { with: PASSWORD_FORMAT }
+  validates :password, format: { with: PASSWORD_FORMAT }, if: :password_required?
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
