423e67132e2059890bb3271a8797d2ce20e71d46
DaniilYusha <ilushenko79@gmail.com>
2020-10-27 18:44:18 +0200

    Remove useless lines after rebasing

--- db/schema.rb
+++ db/schema.rb
@@ -84,42 +84,9 @@ ActiveRecord::Schema.define(version: 20_201_006_092_129) do
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'provider'
-    t.string 'uid'
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-  end
-
-=======
->>>>>>> 2f8cc24... Fix rubocop offenses
   add_foreign_key 'author_books', 'authors'
   add_foreign_key 'author_books', 'books'
   add_foreign_key 'book_materials', 'books'
   add_foreign_key 'book_materials', 'materials'
   add_foreign_key 'books', 'categories'
-<<<<<<< HEAD
-=======
-  add_foreign_key "author_books", "authors"
-  add_foreign_key "author_books", "books"
-  add_foreign_key "book_materials", "books"
-  add_foreign_key "book_materials", "materials"
-  add_foreign_key "books", "categories"
->>>>>>> 05c3ebf... Add users table
-=======
->>>>>>> 2f8cc24... Fix rubocop offenses
 end
