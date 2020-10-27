0c23311572476b62bfe18fc204f611d5b63f36c9
DaniilYusha <ilushenko79@gmail.com>
2020-10-27 12:36:04 +0200

    Remove all specs except http status from pages_controller_spec file

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,27 +1,7 @@
 RSpec.describe PagesController, type: :controller do
   describe 'GET /' do
-    before do
-      create_list(:book, PagesController::LAST_ADDED_BOOKS_COUNT)
-      get :home
-    end
+    before { get :home }
 
-    it 'assings @latest_books' do
-      expect(assigns(:latest_books)).to match_array(
-        BookDecorator.decorate_collection(Book.includes(:authors)
-        .last(PagesController::LAST_ADDED_BOOKS_COUNT))
-      )
-    end
-
-    it 'has current path' do
-      expect(page).to have_current_path root_path
-    end
-
-    it 'returns http success' do
-      expect(response).to have_http_status :success
-    end
-
-    it 'render home/index template' do
-      expect(response).to render_template :index
-    end
+    it { expect(response).to have_http_status :success }
   end
 end
