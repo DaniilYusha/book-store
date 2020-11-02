eaef94f34b68437d2d4be482ed1a1900b2449034
DaniilYusha <ilushenko79@gmail.com>
2020-11-02 16:58:49 +0200

    Add book_params method to permit book id parameter in BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,18 @@
 class BooksController < ApplicationController
 
   def index
-    @pagy, @books = pagy_array(SortedBooksQuery.new(@categories, params).all.decorate)
+    @pagy, @books = pagy_array(SortedBooksQuery.new(@categories, book_params).all.decorate)
     @books_count = Book.count
     @current_category = params[:category_id]
   end
 
   def show
-    @book = Book.includes(:authors).find(params[:id]).decorate
+    @book = Book.includes(:authors).find(book_params[:id]).decorate
+  end
+
+  private
+
+  def book_params
+    params.permit(:id)
   end
 end
