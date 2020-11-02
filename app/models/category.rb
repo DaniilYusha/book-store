f47070134226b2122a6abaac9a1fb86a303c64ba
DaniilYusha <ilushenko79@gmail.com>
2020-11-02 13:38:55 +0200

    Change regex for category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,9 @@
 class Category < ApplicationRecord
+  NAMES_FORMAT_PATTERN = /\A[a-zA-z\s]+\z/.freeze
+
   has_many :books, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, format: { with: NAMES_FORMAT_PATTERN }
 
   scope :with_books, -> { includes [:books] }
 end
