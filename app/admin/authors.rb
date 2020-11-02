3341640605c62bd4eb70263ed46f9aae1352249c
DaniilYusha <ilushenko79@gmail.com>
2020-11-02 13:42:20 +0200

    Register authors page for admin

---
+++ app/admin/authors.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Author do
+  permit_params :first_name, :last_name
+
+  actions :index, :new, :create, :edit, :destroy
+
+  filter :first_name
+  filter :last_name
+  filter :books
+
+  index do
+    selectable_column
+    id_column
+    column :first_name
+    column :last_name
+    actions
+  end
+end
