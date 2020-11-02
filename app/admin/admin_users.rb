d39008d3494d1d19afa7e5232c55c181fc2f356c
DaniilYusha <ilushenko79@gmail.com>
2020-11-02 13:41:30 +0200

    Register admin_users page for admin

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register AdminUser do
+  permit_params :email, :password, :password_confirmation
+
+  filter :email
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+end
