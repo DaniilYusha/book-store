bf84b3da0b621d02f832b01385dc6f5ef693a694
DaniilYusha <ilushenko79@gmail.com>
2020-11-02 13:38:18 +0200

    Change validation of price and relation with authors table in Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,15 +4,16 @@ class Book < ApplicationRecord
   MAX_PRICE = 1_000_000
 
   has_many :author_book, dependent: :destroy
-  has_many :authors, through: :author_book, dependent: :destroy
-  has_many :book_material
+  has_many :authors, through: :author_book
+  has_many :book_material, dependent: :destroy
   has_many :materials, through: :book_material
 
   belongs_to :category
 
   validates :title, presence: true, uniqueness: true, length: { maximum: TITLE_MAX_LENGTH }
   validates :price, presence: true,
-                    numericality: { greater_than: MIN_PRICE, less_than: MAX_PRICE }
+                    numericality: { greater_than_or_equal_to: MIN_PRICE,
+                                    less_than_or_equal_to: MAX_PRICE }
   validates :published_at, presence: true
 
   scope :with_authors, -> { includes [:authors] }
