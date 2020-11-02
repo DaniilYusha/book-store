91ad6065e85e7d3190049c906ba4e10dc40c430c
DaniilYusha <ilushenko79@gmail.com>
2020-11-02 13:36:39 +0200

    Change pattern for authot names

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
   NAME_MAX_LENGTH = 30
-  NAMES_FORMAT_PATTERN = /[a-zA-z]/.freeze
+  NAMES_FORMAT_PATTERN = /\A[a-zA-z.']+\z/.freeze
 
   has_many :author_book, dependent: :destroy
   has_many :books, through: :author_book, dependent: :destroy
