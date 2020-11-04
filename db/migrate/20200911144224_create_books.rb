8c21bb5282d7ec23a71a4f69fc029d63922aa947
DaniilYusha <ilushenko79@gmail.com>
2020-11-04 14:53:49 +0200

    Change belongs_to to references in migrations

--- db/migrate/20200911144224_create_books.rb
+++ db/migrate/20200911144224_create_books.rb
@@ -9,7 +9,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
       t.decimal :depth, precision: 6, scale: 2, null: false
       t.date :published_at, null: false
       t.string :materials, unique: true, null: false
-      t.belongs_to :category, foreign_key: true, index: true
+      t.references :category, foreign_key: true, index: true
 
       t.timestamps
     end
