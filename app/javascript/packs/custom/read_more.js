2f48c184f561801fad0cc50b8904f39288396cbf
DaniilYusha <ilushenko79@gmail.com>
2020-11-04 11:40:18 +0200

    Replace double quotes with single in read_more.js file

--- app/javascript/packs/custom/read_more.js
+++ app/javascript/packs/custom/read_more.js
@@ -1,19 +1,19 @@
 $(document).ready(() => {
-  $(document).on("click", ".read-more", function(event){
+  $(document).on('click', '.read-more', function(event){
     event.preventDefault();
 
-    $(".short-description").toggle();
+    $('.short-description').toggle();
     $(this).toggle();
-    $(".more-description").toggle();
-    $(".read-less").toggle();
+    $('.more-description').toggle();
+    $('.read-less').toggle();
   });
 
-  $(document).on("click", ".read-less", function(event){
+  $(document).on('click', '.read-less', function(event){
     event.preventDefault();
 
-    $(".more-description").toggle();
+    $('.more-description').toggle();
     $(this).toggle();
-    $(".short-description").toggle();
-    $(".read-more").toggle();
+    $('.short-description').toggle();
+    $('.read-more').toggle();
   });
 });
