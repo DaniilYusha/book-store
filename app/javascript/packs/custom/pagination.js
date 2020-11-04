19ea3a2c87fdc7f46c7937f20b29317a7548b352
DaniilYusha <ilushenko79@gmail.com>
2020-11-04 10:53:39 +0200

    Add improvements to custom pagination

--- app/javascript/packs/custom/pagination.js
+++ app/javascript/packs/custom/pagination.js
@@ -1,13 +1,11 @@
 $(document).ready(() => {
-  let limit = +$('#books_count').val();
-  $('.view-more').attr('href', window.location.href);
-
   $(document).on('click', '.view-more', function(event){
     event.preventDefault();
 
-    let url = $(this).attr('href');
+    let limit = +$('#books_count').val();
+    let url = window.location.href;
     let current_books_list = $('.books-list');
-    limit += limit;
+    limit += +$('#books_count').data('per-page');
 
     $.ajax({
       url: url,
@@ -18,6 +16,7 @@ $(document).ready(() => {
       success: function(data){
         let new_books_list = $(data).find('.books-list');
         $(current_books_list).replaceWith(new_books_list);
+        $('#books_count').val(limit)
       }
     });
   });
