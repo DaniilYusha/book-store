656a8bb119a1ffc07a81b16a1cee1eeb82c08fc1
DaniilYusha <ilushenko79@gmail.com>
2020-11-04 11:46:51 +0200

    Remove unnecessary constants from Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,4 @@
 class Author < ApplicationRecord
-  NAME_MAX_LENGTH = 30
-  NAMES_FORMAT_PATTERN = /[a-zA-z]/.freeze
-
   has_many :author_book, dependent: :destroy
   has_many :books, through: :author_book, dependent: :destroy
 end
