0051d3cc415a092ef9bd6c373aa15d974a15069f
DaniilYusha <ilushenko79@gmail.com>
2020-11-04 11:46:13 +0200

    Remove unnecessary constants from Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,5 @@
 class Book < ApplicationRecord
   BOOKS_PER_PAGE = 12
-  TITLE_MAX_LENGTH = 100
-  MIN_PRICE = 0
-  MAX_PRICE = 1_000_000
 
   has_many :author_book, dependent: :destroy
   has_many :authors, through: :author_book, dependent: :destroy
