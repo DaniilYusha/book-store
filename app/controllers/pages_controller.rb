46f69aa0c689a022d82e5a60e586404571496f6f
DaniilYusha <ilushenko79@gmail.com>
2020-11-04 16:58:37 +0200

    Remove NewestBooksQuery class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @newest_books = NewestBooksQuery.call
+    @newest_books = BookDecorator.decorate_collection(
+      Book.last(BookDecorator::NEWEST_BOOKS_COUNT)
+    )
   end
 end
