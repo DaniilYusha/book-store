d44d1dcab8620301d720eed6c17b19e6a3ca03d6
DaniilYusha <ilushenko79@gmail.com>
2020-11-04 09:34:20 +0200

    Remove pae gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  include Pagy::Backend
-
   before_action :set_categories
 
   private
--- config/initializers/pagy.rb
+++
@@ -1,3 +0,0 @@
-require 'pagy/extras/array'
-
-Pagy::VARS[:items] = Book::BOOKS_NUMBER_PER_PAGE
