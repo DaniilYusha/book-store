80721744cc5aad9281fdda80dbc38c109836acea
DaniilYusha <ilushenko79@gmail.com>
2020-11-05 12:19:04 +0200

    Add materails field for book factory

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -6,18 +6,17 @@ FactoryBot.define do
     height { rand(7.0..40.0).floor(2) }
     width { rand(1.0..20.0).floor(2) }
     depth { rand(1.0..7.0).floor(2) }
+    materials { FFaker::Lorem.word }
     published_at { FFaker::Time.date }
     association :category
 
     factory :book_with_associations do
       transient do
         authors_count { 3 }
-        materials_count { 1 }
       end
 
       after(:create) do |book, evaluator|
         create_list(:author, evaluator.authors_count, books: [book])
-        create_list(:material, evaluator.materials_count, books: [book])
         book.reload
       end
     end
