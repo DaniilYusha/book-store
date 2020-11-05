0ab44df5a0166ef79cb3874de0c58a9c2796bcd1
DaniilYusha <ilushenko79@gmail.com>
2020-11-05 09:40:34 +0200

    Add functionality for hiding view-more button

--- app/javascript/packs/custom/pagination.js
+++ app/javascript/packs/custom/pagination.js
@@ -16,7 +16,10 @@ $(document).ready(() => {
       success: function(data){
         let new_books_list = $(data).find('.books-list');
         $(current_books_list).replaceWith(new_books_list);
-        $('#books_count').val(limit)
+        $('#books_count').val(limit);
+        if(+$('#books_count').val() > $('.book').length){
+          $('.view-more').hide();
+        }
       }
     });
   });
