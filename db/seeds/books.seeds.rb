5f8faa568522d52b7ae8c83e7ba36c6bba5ffe8f
DaniilYusha <ilushenko79@gmail.com>
2020-11-05 17:06:03 +0200

    Change books seeds generation method

--- db/seeds/books.seeds.rb
+++ db/seeds/books.seeds.rb
@@ -1,18 +1,17 @@
 after :authors do
   authors = Author.all
-  authors.each do |author|
-    2.times do
-      author.books.create(
-        title: FFaker::Book.title,
-        description: FFaker::Book.description,
-        price: rand(1.0..1000.0).floor(2),
-        height: rand(7.0..40.0).floor(2),
-        width: rand(1.0..20.0).floor(2),
-        depth: rand(1.0..7.0).floor(2),
-        published_at: FFaker::Time.date,
-        materials: FFaker::Lorem.word,
-        category_id: rand(1..Category.count)
-      )
-    end
+  40.times do
+    Book.create(
+      title: FFaker::Book.title,
+      description: FFaker::Book.description,
+      price: rand(1.0..1000.0).floor(2),
+      height: rand(7.0..40.0).floor(2),
+      width: rand(1.0..20.0).floor(2),
+      depth: rand(1.0..7.0).floor(2),
+      published_at: FFaker::Time.date,
+      materials: FFaker::Lorem.word,
+      category_id: rand(1..Category.count),
+      authors: [authors.shuffle.first, authors.shuffle.last]
+    )
   end
 end
