5dd98fa8ffbabd3d18e67b24c1616103f11e17a6
DaniilYusha <ilushenko79@gmail.com>
2020-11-09 10:38:03 +0200

    Add js for rating stars

---
+++ app/javascript/packs/custom/review_rating.js
@@ -0,0 +1,38 @@
+$(document).ready(() => {
+  $(document).on('mouseover', '#stars > svg', function(){
+    let rating_value = +$(this).data('value');
+    let stars = $(this).parent().children('svg');
+
+    stars.each(function(index){
+      if(index < rating_value){
+        $(this).removeClass('rate-empty');
+      }
+    });
+  });
+
+  $(document).on('mouseout', '#stars', function(){
+    $(this).children('svg').each(function(){
+      if(!$(this).hasClass('selected-star')){
+        $(this).addClass('rate-empty');
+      }
+    });
+  });
+
+  $(document).on('click', '#stars > svg', function(){
+    let rating_value = +$(this).data('value');
+    let stars = $(this).parent().children('svg');
+
+    stars.each(function(index){
+      $(this).removeClass('selected-star')
+
+      if(index < rating_value){
+        $(this).addClass('selected-star');
+      }
+      else {
+        $(this).addClass('rate-empty');
+      }
+    });
+
+    $('#review_form_rating').val(rating_value);
+  });
+});
