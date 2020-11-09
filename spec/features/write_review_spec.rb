c93f06b33a0f842c56b28cfaf251d06e0a113b1f
DaniilYusha <ilushenko79@gmail.com>
2020-11-09 10:44:07 +0200

    Add specs for write_review partial

--- spec/features/write_review_spec.rb
+++ spec/features/write_review_spec.rb
@@ -1,26 +1,18 @@
 RSpec.describe 'WriteReview', type: :feature do
-  let(:book) { create(:book_with_associations).decorate }
+  let(:user) { create(:user) }
+  let(:book) { create(:book) }
+  let(:login_page) { Pages::SignIn.new }
   let(:book_page) { Pages::Book.new }
 
-  before { visit book_path(book) }
-
-  it 'has current path' do
-    expect(book_page).to have_current_path book_path(book)
+  before do
+    login_page.load
+    login_page.sign_in_form.authenticate_user(user.email, user.password)
+    book_page.load(id: book.id)
   end
 
-  it 'has write review text' do
-    expect(book_page.write_review).to have_write_review_text
-  end
-
-  it 'has scores' do
-    expect(book_page.write_review).to have_scores
-  end
-
-  it 'has title field' do
-    expect(book_page.write_review).to have_title_field
-  end
-
-  it 'has review field' do
-    expect(book_page.write_review).to have_review_field
-  end
+  it { expect(book_page).to have_current_path book_path(book) }
+  it { expect(book_page.write_review).to have_write_review_text }
+  it { expect(book_page.write_review).to have_scores }
+  it { expect(book_page.write_review).to have_title_field }
+  it { expect(book_page.write_review).to have_review_field }
 end
