65aa889d08e001f5f43ebeae9ba6712ead3b6a1a
DaniilYusha <ilushenko79@gmail.com>
2020-11-11 15:13:11 +0200

    Redo pagination logic

--- app/javascript/packs/custom/pagination.js
+++ app/javascript/packs/custom/pagination.js
@@ -24,10 +24,14 @@ $(document).ready(() => {
 
   function hide_view_more(){
     let category_id = +$('#books_count').data('category-id');
-    let books_in_category = +$(`[data-category-id=${category_id}]`).children('span').text();
     let rendered_books = +$('#books_count').val();
+    let all_books = +$('#all_books').text();
+    let books_in_category = +$(`[data-category-id=${category_id}]`).children('span').text();
 
-    if(rendered_books >= books_in_category){
+    if(isNaN(category_id) && rendered_books >= all_books) {
+      $('.view-more').hide();
+    }
+    else if(Number.isInteger(category_id) && rendered_books >= books_in_category) {
       $('.view-more').hide();
     }
   }
