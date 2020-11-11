f15de11ef136eb84ccf07dfb9407737fbddc93b5
DaniilYusha <ilushenko79@gmail.com>
2020-11-11 15:31:32 +0200

    Fix rubocop offenses

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -270,7 +270,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :facebook, Rails.application.credentials.devise[:facebook][:app_id],
-                             Rails.application.credentials.devise[:facebook][:app_secret]
+                  Rails.application.credentials.devise[:facebook][:app_secret]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
