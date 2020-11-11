b6d5ffb579ac0e8c839bb4e418241c10b3fd5715
DaniilYusha <ilushenko79@gmail.com>
2020-11-11 14:39:55 +0200

    Change requirements in application.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,6 @@ require ("@fortawesome/fontawesome-free/js/all.min")
 require("typeface-open-sans")
 
 require("packs/custom/users/password")
-require("packs/custom/book")
-require("packs/custom/books")
 require("packs/custom/flash")
+require("packs/custom/pagination")
+require("packs/custom/read_more")
