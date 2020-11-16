36112277b0353d9c2555f67a888af6225501e2de
DaniilYusha <ilushenko79@gmail.com>
2020-11-16 12:42:47 +0200

    Add set_cart method to before_action in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_action :set_categories, :set_countries
+  before_action :set_categories, :set_countries, :set_cart
 
   private
 
@@ -11,5 +11,22 @@ class ApplicationController < ActionController::Base
 
   def set_countries
     @countries = ISO3166::Country.all.sort_by(&:name)
+  end
+
+  def set_cart
+    if user_signed_in?
+      @cart = current_user.cart || add_cart_to_user
+    elsif session[:cart_id]
+      @cart = Cart.find_by(id: session[:cart_id])
+    else
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+    @cart = @cart.decorate
+  end
+
+  def add_cart_to_user
+    session[:cart_id] = nil
+    current_user.cart = Cart.create
   end
 end
