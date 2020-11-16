4d113c287d8bed4138824147d989848447b05ffb
DaniilYusha <ilushenko79@gmail.com>
2020-11-16 12:36:13 +0200

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_109_110_020) do
+ActiveRecord::Schema.define(version: 20_201_113_143_602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -94,10 +94,37 @@ ActiveRecord::Schema.define(version: 20_201_109_110_020) do
     t.index ['category_id'], name: 'index_books_on_category_id'
   end
 
+  create_table 'cart_items', force: :cascade do |t|
+    t.integer 'quantity', null: false
+    t.bigint 'cart_id'
+    t.bigint 'book_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['book_id'], name: 'index_cart_items_on_book_id'
+    t.index ['cart_id'], name: 'index_cart_items_on_cart_id'
+  end
+
+  create_table 'carts', force: :cascade do |t|
+    t.bigint 'user_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_carts_on_user_id'
+  end
+
   create_table 'categories', force: :cascade do |t|
     t.string 'name', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  create_table 'coupons', force: :cascade do |t|
+    t.string 'code', null: false
+    t.decimal 'discount', precision: 8, scale: 2, null: false
+    t.boolean 'is_valid', default: true
+    t.bigint 'cart_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['cart_id'], name: 'index_coupons_on_cart_id'
   end
 
   create_table 'reviews', force: :cascade do |t|
@@ -137,6 +164,10 @@ ActiveRecord::Schema.define(version: 20_201_109_110_020) do
   add_foreign_key 'author_books', 'authors'
   add_foreign_key 'author_books', 'books'
   add_foreign_key 'books', 'categories'
+  add_foreign_key 'cart_items', 'books'
+  add_foreign_key 'cart_items', 'carts'
+  add_foreign_key 'carts', 'users'
+  add_foreign_key 'coupons', 'carts'
   add_foreign_key 'reviews', 'books'
   add_foreign_key 'reviews', 'users'
 end
