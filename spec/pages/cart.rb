df1fcd277caf0ec9418d582c207e9e0db236d6a8
DaniilYusha <ilushenko79@gmail.com>
2020-11-16 18:12:23 +0200

    Add cart page class

---
+++ spec/pages/cart.rb
@@ -0,0 +1,8 @@
+module Pages
+  class Cart < SitePrism::Page
+    set_url '/carts/{id}'
+
+    section :orders, Sections::Cart::Orders, '.hidden-xs > table.table'
+    section :order_summary, Sections::Cart::OrderSummary, '.general-order-wrap'
+  end
+end
