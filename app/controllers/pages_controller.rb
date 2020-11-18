cf327dace865ff0ecebd4fd17dd13eb028cd13c9
DaniilYusha <ilushenko79@gmail.com>
2020-11-18 09:39:21 +0200

    Add #checkout_login method to PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
       Book.includes(:authors, title_image_attachment: :blob).last(BookDecorator::NEWEST_BOOKS_COUNT)
     )
   end
+
+  def checkout_login
+    redirect_to checkout_index_path if current_user.present?
+  end
 end
