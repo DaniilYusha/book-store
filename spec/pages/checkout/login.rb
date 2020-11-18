694068024d57faf0ee492f1b9536fc592dc3a504
DaniilYusha <ilushenko79@gmail.com>
2020-11-18 11:29:53 +0200

    Add checkout/login page for spec

---
+++ spec/pages/checkout/login.rb
@@ -0,0 +1,10 @@
+module Pages
+  module Checkout
+    class Login < SitePrism::Page
+      set_url '/checkout_login'
+
+      section :returning_customer, Sections::Checkout::Login::ReturningCustomer, '.col-md-5.mb-40'
+      section :new_customer, Sections::Checkout::Login::NewCustomer, '.col-md-5.mb-60'
+    end
+  end
+end
