263137b1f0ffc76a34fbc617abb0752ccbab6d77
DaniilYusha <ilushenko79@gmail.com>
2020-11-21 12:11:32 +0200

    Add contexts to feature specs where it need

--- spec/features/footer_spec.rb
+++ spec/features/footer_spec.rb
@@ -3,24 +3,26 @@ RSpec.describe 'Footer', type: :feature do
 
   before { home_page.load }
 
-  it { expect(home_page).to have_current_path root_path }
+  context 'with footer partial' do
+    it { expect(home_page).to have_current_path root_path }
 
-  it { expect(home_page.footer).to have_home_link }
-  it { expect(home_page.footer).to have_shop_link }
-  it { expect(home_page.footer).to have_email }
-  it { expect(home_page.footer).to have_phone }
-  it { expect(home_page.footer).to have_facebook_link }
+    it { expect(home_page.footer).to have_home_link }
+    it { expect(home_page.footer).to have_shop_link }
+    it { expect(home_page.footer).to have_email }
+    it { expect(home_page.footer).to have_phone }
+    it { expect(home_page.footer).to have_facebook_link }
 
-  context 'when user signed in' do
-    let(:user) { create(:user) }
-    let(:sign_in_page) { Pages::SignIn.new }
+    context 'when user signed in' do
+      let(:user) { create(:user) }
+      let(:sign_in_page) { Pages::SignIn.new }
 
-    before do
-      sign_in_page.load
-      sign_in_page.sign_in_form.authenticate_user user.email, user.password
+      before do
+        sign_in_page.load
+        sign_in_page.sign_in_form.authenticate_user user.email, user.password
+      end
+
+      it { expect(home_page.footer).to have_orders_link }
+      it { expect(home_page.footer).to have_settings_link }
     end
-
-    it { expect(home_page.footer).to have_orders_link }
-    it { expect(home_page.footer).to have_settings_link }
   end
 end
