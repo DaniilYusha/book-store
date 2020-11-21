b84f9b2a386ef5ee9eca4eab1b85ea2856d7d889
DaniilYusha <ilushenko79@gmail.com>
2020-11-21 13:50:27 +0200

    Dry book description toggle functionality

--- app/javascript/packs/book/read_more.js
+++ app/javascript/packs/book/read_more.js
@@ -2,18 +2,19 @@ $(document).ready(() => {
   $(document).on('click', '.read-more', function(event){
     event.preventDefault();
 
-    $('.short-description').toggle();
-    $(this).toggle();
-    $('.more-description').toggle();
-    $('.read-less').toggle();
+    toggle_book_description($(this), $(this).attr('for'), $(this).next().attr('for'));
   });
 
   $(document).on('click', '.read-less', function(event){
     event.preventDefault();
 
-    $('.more-description').toggle();
-    $(this).toggle();
-    $('.short-description').toggle();
-    $('.read-more').toggle();
+    toggle_book_description($(this), $(this).attr('for'), $(this).prev().attr('for'));
   });
+
+  function toggle_book_description(element, clicked_element_role, opposit_role){
+    $(`.${opposit_role}-description`).toggle();
+    $(element).toggle();
+    $(`.${clicked_element_role}-description`).toggle();
+    $(`.read-${opposit_role}`).toggle();
+  }
 });
