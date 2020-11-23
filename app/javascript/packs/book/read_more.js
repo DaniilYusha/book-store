086b12bf28ee747e1bba76b5aecdc99b4550be1d
DaniilYusha <ilushenko79@gmail.com>
2020-11-23 12:59:50 +0200

    DRY toggle_book_description method

--- app/javascript/packs/book/read_more.js
+++ app/javascript/packs/book/read_more.js
@@ -2,19 +2,19 @@ $(document).ready(() => {
   $(document).on('click', '.read-more', function(event){
     event.preventDefault();
 
-    toggle_book_description($(this), $(this).attr('for'), $(this).next().attr('for'));
+    toggle_book_description($(this), $(this).next().attr('for'));
   });
 
   $(document).on('click', '.read-less', function(event){
     event.preventDefault();
 
-    toggle_book_description($(this), $(this).attr('for'), $(this).prev().attr('for'));
+    toggle_book_description($(this), $(this).prev().attr('for'));
   });
 
-  function toggle_book_description(element, clicked_element_role, opposit_role){
+  function toggle_book_description(element, opposit_role){
     $(`.${opposit_role}-description`).toggle();
     $(element).toggle();
-    $(`.${clicked_element_role}-description`).toggle();
+    $(`.${element.attr('for')}-description`).toggle();
     $(`.read-${opposit_role}`).toggle();
   }
 });
