60da5a734bff80170dbc74b564ce10903ef983ff
DaniilYusha <ilushenko79@gmail.com>
2020-11-23 11:23:13 +0200

    Redo pagination logic

--- app/javascript/packs/catalog/pagination.js
+++ app/javascript/packs/catalog/pagination.js
@@ -2,21 +2,19 @@ $(document).ready(() => {
   $(document).on('click', '.view-more', function(event){
     event.preventDefault();
 
-    let limit = +$('#books_count').val();
     let url = window.location.href;
     let current_books_list = $('.books-list');
-    limit += +$('#books_count').data('per-page');
+    let current_books_count = $('#books_count').val();
 
     $.ajax({
       url: url,
       type: 'GET',
       data: {
-        'limit': limit
+        'current_books_count': current_books_count
       },
       success: function(data){
         let new_books_list = $(data).find('.books-list');
         $(current_books_list).replaceWith(new_books_list);
-        $('#books_count').val(limit);
         hide_view_more()
       }
     });
@@ -24,14 +22,13 @@ $(document).ready(() => {
 
   function hide_view_more(){
     let category_id = +$('#books_count').data('category-id');
-    let rendered_books = +$('#books_count').val();
-    let all_books = +$('#all_books').text();
+    let books_count = $('#books_count').val();
     let books_in_category = +$(`[data-category-id=${category_id}]`).children('span').text();
 
-    if(isNaN(category_id) && rendered_books >= all_books) {
+    if(isNaN(category_id) && books_count == books_in_category) {
       $('.view-more').hide();
     }
-    else if(Number.isInteger(category_id) && rendered_books >= books_in_category) {
+    else if(Number.isInteger(category_id) && books_count == books_in_category) {
       $('.view-more').hide();
     }
   }
