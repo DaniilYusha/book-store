ac6e5f2ef7bd3c718e086de0eb244fe42d62eb01
DaniilYusha <ilushenko79@gmail.com>
2020-11-25 18:38:09 +0200

    Redo AddressesController create method logic and remove update method

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,18 +4,23 @@ class AddressesController < ApplicationController
   ATTRIBUTES = %w[first_name last_name address address_type city zip_code country phone].freeze
 
   def create
-    @form = AddressForm.new(address_form_params)
-    redirect_to(edit_user_registration_path, notice: I18n.t('notice.address.saved')) if @form.submit(current_user)
-  end
+    service = SaveAddressService.new(current_user, billing_address_params, shipping_address_params)
+    service.call
 
-  def update
-    @form = AddressForm.new(address_form_params)
-    redirect_to(edit_user_registration_path, notice: I18n.t('notice.address.updated')) if @form.submit(current_user)
+    if service.success
+      redirect_to(edit_user_registration_path, notice: I18n.t('notice.address.saved'))
+    else
+      render template: 'devise/registrations/edit'
+    end
   end
 
   private
 
-  def address_form_params
-    params.require(:address_form).permit(ATTRIBUTES.map(&:to_sym))
+  def billing_address_params
+    params.require(:address).permit(billing: ATTRIBUTES.map(&:to_sym))
+  end
+
+  def shipping_address_params
+    params.require(:address).permit(shipping: ATTRIBUTES.map(&:to_sym))
   end
 end
