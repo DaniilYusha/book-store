b5141526188cf2b9f1ea91f4d54c5df45d4d498a
DaniilYusha <ilushenko79@gmail.com>
2020-11-25 18:38:52 +0200

    Add changes to specs

--- spec/forms/address_form_spec.rb
+++ spec/forms/address_form_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe AddressForm, type: :model do
-  let(:valid_address) { build(:address) }
-  let(:invalid_address) { build(:address, first_name: '!!!') }
+  subject { described_class.new(user) }
 
-  describe 'validations' do
+  let(:user) { create(:user) }
+
+  context 'with validations' do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:address) }
@@ -18,12 +19,37 @@ RSpec.describe AddressForm, type: :model do
     it { is_expected.to validate_length_of(:zip_code).is_at_most(described_class::ZIP_MAX_LENGTH) }
     it { is_expected.to validate_length_of(:phone).is_at_most(described_class::PHONE_MAX_LENGTH) }
 
-    context 'when validates form successfully' do
-      it { expect(described_class.new(valid_address)).to be_valid }
+    context 'when validates params successfully' do
+      let(:valid_address_params) { attributes_for(:address) }
+
+      it { expect(described_class.new(user, valid_address_params)).to be_valid }
     end
 
-    context 'when fails to validate form' do
-      it { expect(described_class.new(invalid_address)).to be_invalid }
+    context 'when params validation fails' do
+      let(:invalid_address_params) { attributes_for(:address, first_name: '!!!') }
+
+      it { expect(described_class.new(invalid_address_params)).to be_invalid }
+    end
+  end
+
+  describe '#validate' do
+    context 'when params are invalid' do
+      let(:invalid_billing_params) { attributes_for(:address, zip_code: '') }
+      let(:invalid_shipping_params) { attributes_for(:address, address_type: AddressForm::SHIPPING_TYPE, zip_code: '') }
+
+      it 'returns errors hash for billing params' do
+        expect(described_class.new(user, invalid_billing_params).validate.class).to eq(Hash)
+      end
+
+      it 'returns errors hash for shipping params' do
+        expect(described_class.new(user, invalid_shipping_params).validate.class).to eq(Hash)
+      end
+    end
+
+    context 'when params are valid' do
+      let(:valid_params) { attributes_for(:address) }
+
+      it { expect(described_class.new(user, valid_params).validate).to eq(nil) }
     end
   end
 end
