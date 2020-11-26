ba2faa701adf923f8c408c2032a03ed148362923
DaniilYusha <ilushenko79@gmail.com>
2020-11-26 19:02:01 +0200

    Replace create with build in author_decorator_spec

--- spec/decorators/author_decorator_spec.rb
+++ spec/decorators/author_decorator_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe AuthorDecorator do
-  let(:author) { create(:author, first_name: 'Daniil', last_name: 'Yusha').decorate }
+  let(:author) { build(:author, first_name: 'Daniil', last_name: 'Yusha').decorate }
   let(:full_name_result) { 'Daniil Yusha' }
 
   it '#full_name' do
