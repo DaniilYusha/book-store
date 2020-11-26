9f10548d9c989d81269a3adb528038b42df1824f
DaniilYusha <ilushenko79@gmail.com>
2020-11-26 19:08:39 +0200

    Use circle brackets

--- spec/pages/admin/new_admin_user.rb
+++ spec/pages/admin/new_admin_user.rb
@@ -1,7 +1,7 @@
 module Pages
   module Admin
     class NewAdminUser < SitePrism::Page
-      set_url 'admin/admin_users/new'
+      set_url('admin/admin_users/new')
 
       section :fields, Sections::Admin::AdminUserFields, '#main_content'
     end
