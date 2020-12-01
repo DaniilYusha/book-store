a8c2745da42a3e0a276deb608872b0d062bcb298
DaniilYusha <ilushenko79@gmail.com>
2020-12-01 18:30:46 +0200

    Add improvements to admin/authors controller

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -5,25 +5,22 @@ ActiveAdmin.register Author do
 
   controller do
     def create
-      handle_author_params
+      @author = Author.new
+      handle_author_params(:new)
     end
 
     def update
-      handle_author_params
+      @author = Author.find_by(id: params[:id])
+      handle_author_params(:edit)
     end
 
     private
 
-    def handle_author_params
-      service = SaveAuthorService.new(permitted_params)
-      service.call
+    def handle_author_params(view)
+      @service = SaveAuthorService.new(permitted_params)
+      @service.call
 
-      if service.errors.empty?
-        redirect_to admin_authors_path, notice: I18n.t('notice.author.saved')
-      else
-        flash.alert = service.errors.full_messages.to_sentence
-        redirect_back fallback_location: admin_authors_path
-      end
+      @service.errors.empty? ? redirect_to(admin_authors_path, notice: I18n.t('notice.author.saved')) : render(view)
     end
   end
 
@@ -38,4 +35,6 @@ ActiveAdmin.register Author do
     column :last_name
     actions
   end
+
+  form partial: 'form'
 end
