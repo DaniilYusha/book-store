994c5ce6de83b1cefd1b534d20f5a5686e6d3a90
DaniilYusha <ilushenko79@gmail.com>
2020-12-01 17:31:44 +0200

    Create SaveCategoryService

---
+++ app/services/save_category_service.rb
@@ -0,0 +1,19 @@
+class SaveCategoryService
+  attr_reader :errors
+
+  def initialize(params)
+    @id = params[:id]
+    @params = params[:category]
+    @form = CategoryForm.new(params[:category])
+    @errors = []
+  end
+
+  def call
+    category = Category.find_by(id: id) || Category.new
+    form.valid? ? category.update(params) : @errors = form.errors
+  end
+
+  private
+
+  attr_reader :id, :params, :form
+end
