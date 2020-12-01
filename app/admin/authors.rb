6f051e87b620f046d83a2aa29769862c59618b46
DaniilYusha <ilushenko79@gmail.com>
2020-12-01 17:33:48 +0200

    Add changes to controller in admin/categories

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -3,6 +3,30 @@ ActiveAdmin.register Author do
 
   actions :index, :new, :create, :edit, :update, :destroy
 
+  controller do
+    def create
+      handle_author_params
+    end
+
+    def update
+      handle_author_params
+    end
+
+    private
+
+    def handle_author_params
+      service = SaveAuthorService.new(permitted_params)
+      service.call
+
+      if service.errors.empty?
+        redirect_to admin_authors_path, notice: I18n.t('notice.author.saved')
+      else
+        flash.alert = service.errors.full_messages.to_sentence
+        redirect_back fallback_location: admin_authors_path
+      end
+    end
+  end
+
   filter :first_name
   filter :last_name
   filter :books
--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,7 +1,32 @@
 ActiveAdmin.register Category do
   permit_params :name
   actions :index, :new, :create, :edit, :update, :destroy
-  controller { skip_before_action :set_categories }
+
+  controller do
+    skip_before_action :set_categories
+
+    def create
+      handle_category_params
+    end
+
+    def update
+      handle_category_params
+    end
+
+    private
+
+    def handle_category_params
+      service = SaveCategoryService.new(permitted_params)
+      service.call
+
+      if service.errors.empty?
+        redirect_to admin_categories_path, notice: I18n.t('notice.category.saved')
+      else
+        flash.alert = service.errors.full_messages.to_sentence
+        redirect_back fallback_location: admin_categories_path
+      end
+    end
+  end
 
   filter :name
 
