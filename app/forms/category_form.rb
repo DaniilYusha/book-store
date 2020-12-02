768fea5b31b5fe8697c79bb017fd9840771192e8
DaniilYusha <ilushenko79@gmail.com>
2020-12-02 12:20:22 +0200

    Remove category name uniqueness validation

--- app/forms/category_form.rb
+++ app/forms/category_form.rb
@@ -6,11 +6,4 @@ class CategoryForm
   NAME_MAX_LENGTH = 50
 
   validates :name, presence: true, length: { maximum: NAME_MAX_LENGTH }
-  validate :name_uniqueness
-
-  private
-
-  def name_uniqueness
-    errors.add(:name, I18n.t('alert.name.unique')) if Category.where(name: name).exists?
-  end
 end
