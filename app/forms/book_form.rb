325b75b9ebff62558a5408a987d0b08fec6a81f9
DaniilYusha <ilushenko79@gmail.com>
2020-12-02 14:58:08 +0200

    Add authors_presence validation to BookForm

--- app/forms/book_form.rb
+++ app/forms/book_form.rb
@@ -7,9 +7,20 @@ class BookForm
   TITLE_MAX_LENGTH = 100
   DESCRIPTION_MAX_LENGTH = 500
 
+  validate :authors_presence
   validates :category_id, :title, :description, :price, :published_at,
             :height, :width, :depth, :materials, presence: true
   validates :title, length: { maximum: TITLE_MAX_LENGTH }
   validates :description, length: { maximum: DESCRIPTION_MAX_LENGTH }
   validates :price, :height, :width, :depth, numericality: { greater_than: 0 }
+
+  private
+
+  def authors_presence
+    errors.add(:authors, I18n.t('alert.blank')) if author_ids.blank? || author_ids_empty?
+  end
+
+  def author_ids_empty?
+    author_ids.first.empty? && author_ids.size == 1
+  end
 end
