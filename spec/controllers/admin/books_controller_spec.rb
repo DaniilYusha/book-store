9a27bcce79349208d5b98b4a73c0df3bfcdfe314
DaniilYusha <ilushenko79@gmail.com>
2020-12-02 14:58:38 +0200

    Add changes to Admin::BooksController spec

--- spec/controllers/admin/books_controller_spec.rb
+++ spec/controllers/admin/books_controller_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe Admin::BooksController, type: :controller do
   let(:admin) { create(:admin_user) }
-  let(:valid_params) { attributes_for(:book).merge!(category_id: category.id) }
+  let(:valid_params) { attributes_for(:book).merge!(category_id: category.id, author_ids: [author.id]) }
   let(:invalid_params) { attributes_for(:book, title: '') }
   let(:category) { create(:category) }
+  let(:author) { create(:author) }
   let(:book) { create(:book) }
 
   before { sign_in(admin) }
