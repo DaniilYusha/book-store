96389292067fe34fd60ad5cd06544ac7442d4e32
DaniilYusha <ilushenko79@gmail.com>
2020-12-02 11:45:47 +0200

    Create Admin::SaveEntitiesService

---
+++ app/services/admin/save_entities_service.rb
@@ -0,0 +1,24 @@
+module Admin
+  class SaveEntitiesService
+    attr_reader :errors
+
+    ENTITIES = { author: Author, book: Book, category: Category }
+
+    def initialize(entity:, params:)
+      @entity_class = ENTITIES[entity]
+      @id = params[:id]
+      @params = params[ENTITIES.key(@entity_class)]
+      @errors = []
+    end
+
+    def call
+      form = "#{entity_class}Form".constantize.new(params)
+      entity = entity_class.find_by(id: id) || entity_class.new
+      form.valid? ? entity.update(params) : @errors = form.errors
+    end
+
+    private
+
+    attr_reader :entity_class, :id, :params
+  end
+end
