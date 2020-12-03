7ebdbc1d5d59fb9e252350a45a78776759a47890
DaniilYusha <ilushenko79@gmail.com>
2020-12-03 14:35:29 +0200

    Add changes to #update_resource method in RegistrationsController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,18 +10,22 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  protected
+
   def update_resource(resource, params)
-    without_password? ? resource.update_without_password(params) : super(resource, params)
+    return super unless params.key?(:email)
+
+    resource.update_without_password(params)
   end
+
+  # def after_update_path_for(resource)
+  #   edit_user_registration_path
+  # end
 
   private
 
   def set_forms
     @billing_form = AddressForm.new(current_user.billing_address)
     @shipping_form = AddressForm.new(current_user.shipping_address)
-  end
-
-  def without_password?
-    params[resource_name][:password].blank? && params[resource_name].key?(:email)
   end
 end
