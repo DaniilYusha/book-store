28cce22f2fb7b66384f922b71dbb99442125bf70
DaniilYusha <ilushenko79@gmail.com>
2020-12-06 15:42:27 +0200

    Add update method and redo create method logic

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,26 +1,48 @@
 class AddressesController < ApplicationController
   before_action :authenticate_user!
+  helper_method :resource_name, :resource, :devise_mapping
 
   ATTRIBUTES = %w[first_name last_name address address_type city zip_code country phone].freeze
 
   def create
-    service = SaveAddressService.new(current_user, billing_address_params, shipping_address_params)
+    service = SaveAddressService.new(user: current_user, params: address_params)
     service.call
-
     if service.success
       redirect_to(edit_user_registration_path, notice: I18n.t('notice.address.saved'))
     else
-      render template: 'devise/registrations/edit'
+      @billing_address = current_user.billing_address || Address.new
+      @shipping_address = current_user.shipping_address || Address.new
+      render 'devise/registrations/edit'
+    end
+  end
+
+  def update
+    service = SaveAddressService.new(user: current_user, params: address_params, id: params[:id])
+    service.call
+    if service.success
+      redirect_to(edit_user_registration_path, notice: I18n.t('notice.address.saved'))
+    else
+      @billing_address = current_user.billing_address || Address.new
+      @shipping_address = current_user.shipping_address || Address.new
+      render 'devise/registrations/edit'
     end
   end
 
   private
 
-  def billing_address_params
-    params.require(:address).permit(billing: ATTRIBUTES.map(&:to_sym))
+  def address_params
+    params.require(:address).permit(ATTRIBUTES.map(&:to_sym)).merge(addressable: current_user)
   end
 
-  def shipping_address_params
-    params.require(:address).permit(shipping: ATTRIBUTES.map(&:to_sym))
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= current_user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
