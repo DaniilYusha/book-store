cd43ea906e58c7517c2c628c970f2cf88b2f1bf8
DaniilYusha <ilushenko79@gmail.com>
2020-12-12 15:17:37 +0200

    Fix rubocop offenses

--- spec/services/save_address_service_spec.rb
+++ spec/services/save_address_service_spec.rb
@@ -6,39 +6,35 @@ RSpec.describe SaveAddressService do
   let(:valid_shipping_params) { attributes_for(:address, address_type: 'shipping') }
 
   describe '#call' do
-    context 'when create billing address' do
-      context 'with invalid params' do
-        it 'returns Hash with errors' do
-          expect(described_class.new(user: user, params: invalid_billing_params).call.class).to eq(Hash)
-        end
-
-        it 'returns Hash with two errors' do
-          expect(described_class.new(user: user, params: invalid_billing_params).call.count).to eq(2)
-        end
-      end
-
-      context 'with valid params' do
-        it 'returns true' do
-          expect(described_class.new(user: user, params: valid_billing_params).call).to eq(true)
-        end
+    context 'with valid billing params' do
+      it 'returns true' do
+        expect(described_class.new(user: user, params: valid_billing_params).call).to eq(true)
       end
     end
 
-    context 'when create shipping address' do
-      context 'with invalid params' do
-        it 'returns Hash with errors' do
-          expect(described_class.new(user: user, params: invalid_shipping_params).call.class).to eq(Hash)
-        end
-
-        it 'returns Hash with one error' do
-          expect(described_class.new(user: user, params: invalid_shipping_params).call.count).to eq(1)
-        end
+    context 'with invalid billing params' do
+      it 'returns Hash with errors' do
+        expect(described_class.new(user: user, params: invalid_billing_params).call.class).to eq(Hash)
       end
 
-      context 'with valid params' do
-        it 'returns true' do
-          expect(described_class.new(user: user, params: valid_shipping_params).call).to eq(true)
-        end
+      it 'returns Hash with two errors' do
+        expect(described_class.new(user: user, params: invalid_billing_params).call.count).to eq(2)
+      end
+    end
+
+    context 'with valid shipping params' do
+      it 'returns true' do
+        expect(described_class.new(user: user, params: valid_shipping_params).call).to eq(true)
+      end
+    end
+
+    context 'with invalid shipping params' do
+      it 'returns Hash with errors' do
+        expect(described_class.new(user: user, params: invalid_shipping_params).call.class).to eq(Hash)
+      end
+
+      it 'returns Hash with one error' do
+        expect(described_class.new(user: user, params: invalid_shipping_params).call.count).to eq(1)
       end
     end
   end
