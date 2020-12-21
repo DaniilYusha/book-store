486df6292662e976856767791099bc11d05b8ecf
DaniilYusha <ilushenko79@gmail.com>
2020-12-21 14:49:54 +0200

    Rename SaveAddressService to PersistAddressService and refactor that

---
+++ app/services/persist_address_service.rb
@@ -0,0 +1,29 @@
+class PersistAddressService
+  attr_reader :errors
+
+  def initialize(user:, params:)
+    @user = user
+    @form = AddressForm.new(params)
+    @params = params.merge(addressable: user)
+    @errors = {}
+  end
+
+  def call
+    return save_address if form.valid?
+
+    @errors = form.errors
+    false
+  end
+
+  private
+
+  attr_reader :user, :params, :form
+
+  def address
+    @address ||= Address.find_by(addressable_id: user.id, address_type: params[:address_type])
+  end
+
+  def save_address
+    address.nil? ? Address.create(params) : address.update(params)
+  end
+end
