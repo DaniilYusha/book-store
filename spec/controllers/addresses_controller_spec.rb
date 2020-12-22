79dd9c39749dcccb4f37141dce8f839b948162c9
DaniilYusha <ilushenko79@gmail.com>
2020-12-22 21:01:17 +0200

    Dry address_controller_spec describe

--- spec/controllers/addresses_controller_spec.rb
+++ spec/controllers/addresses_controller_spec.rb
@@ -1,20 +1,20 @@
 RSpec.describe AddressesController, type: :controller do
   let(:user) { create(:user) }
-  let(:valid_params) { attributes_for(:address) }
-  let(:invalid_params) { attributes_for(:address, city: '') }
 
   before { sign_in(user) }
 
   describe 'POST /addresses' do
+    before { post :create, params: { address: params } }
+
     context 'when params are valid' do
-      before { post :create, params: { address: valid_params } }
+      let(:params) { attributes_for(:address) }
 
       it { expect(response).to redirect_to(settings_path) }
       it { expect(response).to have_http_status(:found) }
     end
 
     context 'when params are invalid' do
-      before { post :create, params: { address: invalid_params } }
+      let(:params) { attributes_for(:address, city: '') }
 
       it { expect(response).to have_http_status(:ok) }
     end
