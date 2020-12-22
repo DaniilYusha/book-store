42127b1edea1e167ff0545c6183e0709513a18eb
DaniilYusha <ilushenko79@gmail.com>
2020-12-22 16:18:04 +0200

    Refactor SaveEntitiesService

--- app/services/admin/save_entities_service.rb
+++ app/services/admin/save_entities_service.rb
@@ -2,23 +2,33 @@ module Admin
   class SaveEntitiesService
     attr_reader :errors
 
-    ENTITIES = { author: Author, book: Book, category: Category }.freeze
+    ENTITIES = {
+      author: Author,
+      book: Book,
+      category: Category
+    }.freeze
 
     def initialize(entity:, params:)
       @entity_class = ENTITIES[entity]
       @id = params[:id]
       @params = params[ENTITIES.key(@entity_class)]
-      @errors = []
+      @form = "#{@entity_class}Form".constantize.new(@params)
+      @errors = {}
     end
 
     def call
-      form = "#{entity_class}Form".constantize.new(params)
-      entity = entity_class.find_by(id: id) || entity_class.new
-      form.valid? ? entity.update(params) : @errors = form.errors
+      return save_entity if form.valid?
+
+      @errors = form.errors
+      false
     end
 
     private
 
-    attr_reader :entity_class, :id, :params
+    attr_reader :entity_class, :id, :params, :form
+
+    def save_entity
+      id.nil? ? entity_class.create(params) : entity_class.find_by(id: id).update(params)
+    end
   end
 end
