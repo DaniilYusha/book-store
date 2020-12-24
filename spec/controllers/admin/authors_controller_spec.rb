de5af40da596afd98270b8759d4c756d26fb9aff
DaniilYusha <ilushenko79@gmail.com>
2020-12-24 16:40:30 +0200

    Dry spec/controllers/admin/authors_controller_spec.rb

--- spec/controllers/admin/authors_controller_spec.rb
+++ spec/controllers/admin/authors_controller_spec.rb
@@ -1,48 +1,41 @@
 RSpec.describe Admin::AuthorsController, type: :controller do
   let(:admin) { create(:admin_user) }
-  let(:valid_params) { attributes_for(:author) }
-  let(:invalid_params) { attributes_for(:category, first_name: '') }
 
   before { sign_in(admin) }
 
   describe 'POST create' do
-    context 'with valid params' do
-      it 'creates a new Author' do
-        expect { post :create, params: { author: valid_params } }.to change(Author, :count).by(1)
-      end
+    before { post :create, params: { author: params } }
 
-      it 'redirects to authors list' do
-        post :create, params: { author: valid_params }
-        expect(response).to have_http_status :found
-      end
+    context 'with valid params' do
+      let(:params) { attributes_for(:author) }
+
+      it { expect(response).to redirect_to(admin_authors_path) }
+      it { expect(response).to have_http_status(:found) }
     end
 
     context 'with invalid params' do
-      before { post :create, params: { author: invalid_params } }
+      let(:params) { attributes_for(:category, first_name: '') }
 
-      it 'invalid_params return http success' do
-        expect(response).to have_http_status :success
-      end
+      it { expect(response).to have_http_status :success }
     end
   end
 
   describe 'PUT update' do
     let(:author) { create(:author) }
 
-    context 'with valid params' do
-      before { put :update, params: { id: author.id, author: valid_params } }
+    before { put :update, params: { id: author.id, author: params } }
 
-      it 'returns http status redirect' do
-        expect(response).to have_http_status :redirect
-      end
+    context 'with valid params' do
+      let(:params) { attributes_for(:author) }
+
+      it { expect(response).to redirect_to(admin_authors_path) }
+      it { expect(response).to have_http_status(:redirect) }
     end
 
     context 'with invalid params' do
-      before { put :update, params: { id: author.id, author: invalid_params } }
+      let(:params) { attributes_for(:category, first_name: '') }
 
-      it 'returns http status redirect' do
-        expect(response).to have_http_status :success
-      end
+      it { expect(response).to have_http_status(:success) }
     end
   end
 end
