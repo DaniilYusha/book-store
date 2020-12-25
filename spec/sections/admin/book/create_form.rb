8f85b9d69b342daa317ae43c74f0be9559005a03
DaniilYusha <ilushenko79@gmail.com>
2020-12-25 19:08:42 +0200

    Add #submit method to Admin::Book::CreateForm section

--- spec/sections/admin/book/create_form.rb
+++ spec/sections/admin/book/create_form.rb
@@ -2,18 +2,33 @@ module Sections
   module Admin
     module Book
       class CreateForm < SitePrism::Section
-        element :category_input, '#book_category_input'
-        element :authors_input, '#book_authors_input'
-        element :title_input, '#book_title_input'
-        element :description_input, '#book_description_input'
-        element :price_input, '#book_price_input'
-        element :published_at_input, '#book_published_at_input'
-        element :height_input, '#book_height_input'
-        element :width_input, '#book_width_input'
-        element :depth_input, '#book_depth_input'
-        element :materials_input, '#book_materials_input'
+        element :category_select, '#book_category_id'
+        elements :authors_checkboxes, '.choices-group > li > label > input'
+        element :title_input, '#book_title'
+        element :description_input, '#book_description'
+        element :price_input, '#book_price'
+        element :published_at_input, '#book_published_at'
+        element :height_input, '#book_height'
+        element :width_input, '#book_width'
+        element :depth_input, '#book_depth'
+        element :materials_input, '#book_materials'
         element :create_button, '#book_submit_action > input'
         element :cancel_button, '.cancel'
+
+        def submit(params)
+          category_select.select(params[:category])
+          authors_checkboxes.first.set(true)
+          fill_in_fields(params.except(:category))
+          create_button.click
+        end
+
+        private
+
+        def fill_in_fields(params)
+          fields = [title_input, description_input, price_input, height_input,
+                    width_input, depth_input, materials_input, published_at_input]
+          fields.zip(params.values).to_h.each { |field, value| field.set(value) }
+        end
       end
     end
   end
