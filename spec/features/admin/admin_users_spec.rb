f70b804aab087e44796c9d6e02e36cfd00ce3d75
DaniilYusha <ilushenko79@gmail.com>
2020-12-25 15:31:38 +0200

    Add specs for EditAdminUser page

--- spec/features/admin/admin_users_spec.rb
+++ spec/features/admin/admin_users_spec.rb
@@ -12,11 +12,13 @@ RSpec.describe 'admin/admin_users', type: :feature do
 
     before { admins_page.load }
 
-    it { expect(admins_page.main_content).to have_id_column }
-    it { expect(admins_page.main_content).to have_name_column }
+    context 'with page elements' do
+      it { expect(admins_page.main_content).to have_id_column }
+      it { expect(admins_page.main_content).to have_name_column }
 
-    it { expect(admins_page.main_content).to have_content(admin.id) }
-    it { expect(admins_page.main_content).to have_content(admin.email) }
+      it { expect(admins_page.main_content).to have_content(admin.id) }
+      it { expect(admins_page.main_content).to have_content(admin.email) }
+    end
   end
 
   describe '/new' do
@@ -41,7 +43,7 @@ RSpec.describe 'admin/admin_users', type: :feature do
       before { new_admin_page.fields.submit(admin_params[:email], admin_params[:password]) }
 
       it { expect(new_admin_page.text).to have_content(admin_params[:email]) }
-      it { expect(new_admin_page.text).to have_content(I18n.t('notice.admin.saved')) }
+      it { expect(new_admin_page.text).to have_content(I18n.t('notice.admin.created')) }
     end
 
     context 'when fill in form with invalid params' do
@@ -65,25 +67,72 @@ RSpec.describe 'admin/admin_users', type: :feature do
 
     before { admin_page.load(id: admin.id) }
 
-    it { expect(admin_page.admin_details).to have_header }
-    it { expect(admin_page.admin_details).to have_email_row }
-    it { expect(admin_page.admin_details).to have_reset_password_token_list_row }
-    it { expect(admin_page.admin_details).to have_reset_password_sent_at_row }
-    it { expect(admin_page.admin_details).to have_remember_created_at_row }
-    it { expect(admin_page.admin_details).to have_created_at_row }
-    it { expect(admin_page.admin_details).to have_updated_at_row }
+    context 'with page elements' do
+      it { expect(admin_page.admin_details).to have_header }
+      it { expect(admin_page.admin_details).to have_email_row }
+      it { expect(admin_page.admin_details).to have_reset_password_token_list_row }
+      it { expect(admin_page.admin_details).to have_reset_password_sent_at_row }
+      it { expect(admin_page.admin_details).to have_remember_created_at_row }
+      it { expect(admin_page.admin_details).to have_created_at_row }
+      it { expect(admin_page.admin_details).to have_updated_at_row }
 
-    it { expect(admin_page.admin_details.email_row.text).to have_content(admin.email) }
-    it { expect(admin_page.admin_details.remember_created_at_row.text).to have_content(admin.remember_created_at) }
+      it { expect(admin_page.admin_details.email_row.text).to have_content(admin.email) }
+      it { expect(admin_page.admin_details.remember_created_at_row.text).to have_content(admin.remember_created_at) }
 
-    it {
-      expect(admin_page.admin_details.reset_password_token_list_row.text)
-        .to have_content(admin.reset_password_token)
-    }
+      it {
+        expect(admin_page.admin_details.reset_password_token_list_row.text)
+          .to have_content(admin.reset_password_token)
+      }
 
-    it {
-      expect(admin_page.admin_details.reset_password_sent_at_row.text)
-        .to have_content(admin.reset_password_sent_at)
-    }
+      it {
+        expect(admin_page.admin_details.reset_password_sent_at_row.text)
+          .to have_content(admin.reset_password_sent_at)
+      }
+    end
+  end
+
+  describe '/edit' do
+    let(:admin_user) { create(:admin_user) }
+    let(:edit_admin_page) { Pages::Admin::EditAdminUser.new }
+
+    before { edit_admin_page.load(id: admin_user.id) }
+
+    context 'with page elements' do
+      it { expect(edit_admin_page.fields).to have_email_row }
+      it { expect(edit_admin_page.fields).to have_email_input }
+      it { expect(edit_admin_page.fields.email_input.value).to have_content(admin_user.email) }
+      it { expect(edit_admin_page.fields).to have_password_row }
+      it { expect(edit_admin_page.fields).to have_password_input }
+      it { expect(edit_admin_page.fields.password_input.value).not_to have_content(admin_user.password) }
+      it { expect(edit_admin_page.fields).to have_password_confirmation_row }
+      it { expect(edit_admin_page.fields).to have_password_confirmation_input }
+      it { expect(edit_admin_page.fields.password_confirmation_input.value).not_to have_content(admin_user.password) }
+      it { expect(edit_admin_page.fields).to have_create_button }
+      it { expect(edit_admin_page.fields).to have_cancel_button }
+    end
+
+    context 'when fill in form with valid params' do
+      let(:admin_params) { attributes_for(:admin_user) }
+
+      before { edit_admin_page.fields.submit(admin_params[:email], admin_params[:password]) }
+
+      it { expect(edit_admin_page.text).to have_content(admin_params[:email]) }
+      it { expect(edit_admin_page.text).to have_content(I18n.t('notice.admin.updated')) }
+    end
+
+    context 'when fill in form with invalid params' do
+      let(:admin_params) { attributes_for(:admin_user, email: '') }
+
+      before { edit_admin_page.fields.submit(admin_params[:email], admin_params[:password]) }
+
+      it { expect(edit_admin_page.fields.email_row.text).to have_content(I18n.t('alert.blank')) }
+      it { expect(edit_admin_page.fields.email_input.value).to have_content(admin_params[:email]) }
+      it { expect(edit_admin_page.fields.password_input.value).not_to have_content(admin_params[:password]) }
+
+      it {
+        expect(edit_admin_page.fields.password_confirmation_input.value)
+          .not_to have_content(admin_params[:password])
+      }
+    end
   end
 end
