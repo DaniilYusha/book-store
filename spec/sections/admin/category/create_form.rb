3cee4b37693f03ac22c0303270ca565c8633e953
DaniilYusha <ilushenko79@gmail.com>
2020-12-25 16:28:12 +0200

    Change selector for name_input element and add #submit method

--- spec/sections/admin/category/create_form.rb
+++ spec/sections/admin/category/create_form.rb
@@ -2,9 +2,14 @@ module Sections
   module Admin
     module Category
       class CreateForm < SitePrism::Section
-        element :name_input, '#category_name_input'
+        element :name_input, '#category_name'
         element :create_button, '#category_submit_action > input'
         element :cancel_button, '.cancel'
+
+        def submit(params)
+          name_input.set(params[:name])
+          create_button.click
+        end
       end
     end
   end
