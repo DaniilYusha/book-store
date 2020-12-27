b362f10d157d13f8c658e3c2bbc195aaea1a7c50
DaniilYusha <ilushenko79@gmail.com>
2020-12-27 12:27:31 +0200

    include Warden::Test::Helpers module in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,8 @@ end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include Warden::Test::Helpers
+  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
