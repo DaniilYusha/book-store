220a45c436c04ba0b4d4f6fa0b35efaad95a2768
DaniilYusha <ilushenko79@gmail.com>
2020-12-28 20:27:42 +0200

    Fix rubocop offenses

--- app/services/admin/persist_entities_service.rb
+++ app/services/admin/persist_entities_service.rb
@@ -28,7 +28,7 @@ module Admin
     attr_reader :entity_class, :id, :params, :form
 
     def persist_entity
-      id ? entity_class.find_by(id: id).update(params): entity_class.create(params)
+      id ? entity_class.find_by(id: id).update(params) : entity_class.create(params)
     end
   end
 end
