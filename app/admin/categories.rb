4a0b43b4612ba2e96a76ade16f154e054d4caebb
DaniilYusha <ilushenko79@gmail.com>
2020-12-28 13:30:31 +0200

    Add all controller methods except #show in admin/categories

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Category do
   permit_params :name
-  actions :index, :new, :create, :edit, :update, :destroy
+
+  actions :all, except: :show
 
   controller do
     skip_before_action :set_categories
