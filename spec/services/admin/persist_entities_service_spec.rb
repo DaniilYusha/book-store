03e89dc770af15046d13c1fd0381cdca793f4242
DaniilYusha <ilushenko79@gmail.com>
2020-12-29 17:14:47 +0200

    Rename contexts in admin/persist_entities_service_spec

--- spec/services/admin/persist_entities_service_spec.rb
+++ spec/services/admin/persist_entities_service_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Admin::PersistEntitiesService do
     context 'when save author with valid params' do
       let(:valid_author_params) { { author: attributes_for(:author) } }
 
-      it 'returns new author' do
+      it 'returns true' do
         expect(described_class.new(entity: :author, params: valid_author_params).call).to be true
       end
     end
@@ -21,7 +21,7 @@ RSpec.describe Admin::PersistEntitiesService do
       let(:category) { create(:category) }
       let(:valid_book_params) { { book: attributes_for(:book, category_id: category.id, author_ids: [author.id]) } }
 
-      it 'returns new book' do
+      it 'returns true' do
         expect(described_class.new(entity: :book, params: valid_book_params).call).to be true
       end
     end
@@ -37,7 +37,7 @@ RSpec.describe Admin::PersistEntitiesService do
     context 'when save category with valid params' do
       let(:valid_category_params) { { category: attributes_for(:category) } }
 
-      it 'returns new category' do
+      it 'returns true' do
         expect(described_class.new(entity: :category, params: valid_category_params).call).to be true
       end
     end
@@ -45,7 +45,7 @@ RSpec.describe Admin::PersistEntitiesService do
     context 'when save category with invalid params' do
       let(:invalid_category_params) { { category: attributes_for(:category, name: '') } }
 
-      it 'returns new category' do
+      it 'returns false' do
         expect(described_class.new(entity: :category, params: invalid_category_params).call).to be false
       end
     end
