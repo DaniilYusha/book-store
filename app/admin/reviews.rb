eaa1f4b727553748ec2398aeb1e3aec41633040a
DaniilYusha <ilushenko79@gmail.com>
2020-12-31 13:30:57 +0200

    Add reject and approve batch actions for admin/reviews

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -5,11 +5,12 @@ ActiveAdmin.register Review do
 
   actions :index, :show
 
-  filter :book
-  filter :title
-  filter :created_at
-  filter :user
-  filter :status
+  scope :unprocessed, default: true
+  scope :approved
+  scope :rejected
+
+  config.filters = false
+  config.batch_actions = true
 
   index do
     selectable_column
@@ -22,6 +23,16 @@ ActiveAdmin.register Review do
     actions
   end
 
+  batch_action :approve, if: proc { @current_scope.scope_method != :approved } do |ids|
+    batch_action_collection.find(ids).each(&:approved!)
+    redirect_back(fallback_location: admin_reviews_path)
+  end
+
+  batch_action :reject, if: proc { @current_scope.scope_method != :rejected } do |ids|
+    batch_action_collection.find(ids).each(&:rejected!)
+    redirect_back(fallback_location: admin_reviews_path)
+  end
+
   action_item :approve, only: :show do
     link_to 'Approve', approve_admin_review_path(review), method: :put unless review.approved?
   end
@@ -31,14 +42,14 @@ ActiveAdmin.register Review do
   end
 
   member_action :approve, method: :put do
-    review = Review.find(params[:id])
-    review.update(status: 1)
+    review = Review.find(permitted_params[:id])
+    review.update(status: :approved)
     redirect_to admin_review_path(review)
   end
 
   member_action :reject, method: :put do
-    review = Review.find(params[:id])
-    review.update(status: 2)
+    review = Review.find(permitted_params[:id])
+    review.update(status: :rejected)
     redirect_to admin_review_path(review)
   end
 end
