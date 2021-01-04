340cb37bc6637560276d1b29018541ad0dcbbaf1
DaniilYusha <ilushenko79@gmail.com>
2021-01-04 18:18:59 +0200

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_029_080_037) do
+ActiveRecord::Schema.define(version: 20_210_104_155_425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+  end
+
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.bigint 'byte_size', null: false
+    t.string 'checksum', null: false
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  end
 
   create_table 'addresses', force: :cascade do |t|
     t.string 'first_name', null: false
@@ -59,24 +80,65 @@ ActiveRecord::Schema.define(version: 20_201_029_080_037) do
   end
 
   create_table 'books', force: :cascade do |t|
-    t.string 'title', null: false
+    t.string 'title'
     t.text 'description', default: ''
-    t.decimal 'price', precision: 8, scale: 2, null: false
-    t.decimal 'height', precision: 6, scale: 2, null: false
-    t.decimal 'width', precision: 6, scale: 2, null: false
-    t.decimal 'depth', precision: 6, scale: 2, null: false
-    t.date 'published_at', null: false
-    t.string 'materials', null: false
+    t.decimal 'price'
+    t.decimal 'height'
+    t.decimal 'width'
+    t.decimal 'depth'
+    t.date 'published_at'
+    t.string 'materials'
     t.bigint 'category_id'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.index ['category_id'], name: 'index_books_on_category_id'
   end
 
+  create_table 'cart_items', force: :cascade do |t|
+    t.integer 'quantity', null: false
+    t.bigint 'cart_id'
+    t.bigint 'book_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['book_id'], name: 'index_cart_items_on_book_id'
+    t.index ['cart_id'], name: 'index_cart_items_on_cart_id'
+  end
+
+  create_table 'carts', force: :cascade do |t|
+    t.bigint 'user_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_carts_on_user_id'
+  end
+
   create_table 'categories', force: :cascade do |t|
     t.string 'name', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  create_table 'coupons', force: :cascade do |t|
+    t.string 'code', null: false
+    t.decimal 'discount', precision: 8, scale: 2, null: false
+    t.boolean 'is_valid', default: true
+    t.bigint 'cart_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['cart_id'], name: 'index_coupons_on_cart_id'
+  end
+
+  create_table 'reviews', force: :cascade do |t|
+    t.string 'title', null: false
+    t.text 'text', null: false
+    t.integer 'rating', null: false
+    t.integer 'status', default: 0, null: false
+    t.boolean 'is_verified', default: false
+    t.bigint 'user_id'
+    t.bigint 'book_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['book_id'], name: 'index_reviews_on_book_id'
+    t.index ['user_id'], name: 'index_reviews_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -98,7 +160,14 @@ ActiveRecord::Schema.define(version: 20_201_029_080_037) do
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
   add_foreign_key 'author_books', 'authors'
   add_foreign_key 'author_books', 'books'
   add_foreign_key 'books', 'categories'
+  add_foreign_key 'cart_items', 'books'
+  add_foreign_key 'cart_items', 'carts'
+  add_foreign_key 'carts', 'users'
+  add_foreign_key 'coupons', 'carts'
+  add_foreign_key 'reviews', 'books'
+  add_foreign_key 'reviews', 'users'
 end
