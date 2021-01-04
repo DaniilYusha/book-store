9108fefcf254d7a88b51bb60274dfecdda2d2574
DaniilYusha <ilushenko79@gmail.com>
2021-01-04 19:53:51 +0200

    Remove some contexts from admin/books and admin/categories controllers

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -1,7 +1,5 @@
 RSpec.describe Admin::CategoriesController, type: :controller do
   let(:admin) { create(:admin_user) }
-  let(:valid_params) { attributes_for(:category) }
-  let(:invalid_params) { attributes_for(:category, name: '') }
 
   before { sign_in(admin) }
 
@@ -13,12 +11,6 @@ RSpec.describe Admin::CategoriesController, type: :controller do
 
       it { expect(response).to redirect_to(admin_categories_path) }
       it { expect(response).to have_http_status(:found) }
-    end
-
-    context 'with invalid params' do
-      let(:params) { attributes_for(:category, name: '') }
-
-      it { expect(response).to have_http_status(:success) }
     end
   end
 
@@ -32,12 +24,6 @@ RSpec.describe Admin::CategoriesController, type: :controller do
 
       it { expect(response).to redirect_to(admin_categories_path) }
       it { expect(response).to have_http_status(:redirect) }
-    end
-
-    context 'with invalid params' do
-      let(:params) { attributes_for(:category, name: '') }
-
-      it { expect(response).to have_http_status(:success) }
     end
   end
 end
