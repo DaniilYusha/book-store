8018224c480aa36f8e86491383c23ee445514b08
DaniilYusha <ilushenko79@gmail.com>
2021-01-08 17:00:53 +0200

    Check values for status enum

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -18,6 +18,6 @@ RSpec.describe Review, type: :model do
   end
 
   describe 'enum for status' do
-    it { is_expected.to define_enum_for(:status) }
+    it { is_expected.to define_enum_for(:status).with_values(unprocessed: 0, approved: 1, rejected: 2) }
   end
 end
