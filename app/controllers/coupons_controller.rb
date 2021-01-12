2c4566a303683bd47f699e139c2e5317a726e59a
DaniilYusha <ilushenko79@gmail.com>
2021-01-12 14:32:33 +0200

    Change update method in CouponsController

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,14 +1,18 @@
 class CouponsController < ApplicationController
-  def check
-    service = CouponService.new(coupon_params[:code], @cart)
-    service.call
-    service.errors.any? ? flash[:alert] = service.errors.first : flash[:notice] = I18n.t('notice.coupon.apply')
-    redirect_to(cart_path(@cart))
+  def update
+    service = UpdateCouponService.new(coupon_params, @cart)
+    if service.call
+      flash[:notice] = I18n.t('notice.coupon.apply')
+    else
+      presenter = CouponPresenter.new(errors: service.errors)
+      flash[:alert] = presenter.errors
+    end
+    redirect_to(carts_path)
   end
 
   private
 
   def coupon_params
-    params.require(:coupon).permit(:code).merge(cart_id: @cart.id)
+    params.require(:coupon).permit(:code)
   end
 end
