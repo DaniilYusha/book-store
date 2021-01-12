caaaf74613b80adc8689c13a88699ed9c56a0807
DaniilYusha <ilushenko79@gmail.com>
2021-01-12 14:30:13 +0200

    Change dependency on cart destroy to nullify with coupon model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-  has_one :coupon, dependent: :destroy
+  has_one :coupon, dependent: :nullify
 
   belongs_to :user, optional: true
 end
