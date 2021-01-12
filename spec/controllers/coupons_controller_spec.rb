df5a0023e3aa43f882644ed59efb74682c40148d
DaniilYusha <ilushenko79@gmail.com>
2021-01-12 15:40:33 +0200

    Fix rubocop offenses

--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CouponsController, type: :controller do
     before { patch :update, params: params }
 
     context 'with valid params' do
-      let(:params) { { coupon: { code: coupon.code} } }
+      let(:params) { { coupon: { code: coupon.code } } }
 
       it { expect(response).to have_http_status(:redirect) }
       it { expect(flash[:notice]).to eq(I18n.t('notice.coupon.apply')) }
