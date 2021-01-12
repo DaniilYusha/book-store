a1fadeb9c275b9672915fcede461ecf97cf0d266
DaniilYusha <ilushenko79@gmail.com>
2021-01-12 14:39:49 +0200

    Add update method to CartItemsController and change create method

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,17 +1,27 @@
 class CartItemsController < ApplicationController
   def create
-    CartItemService.new(cart_item_params).call
-    redirect_back fallback_location: root_path
+    PersistCartItemService.new(params: cart_item_params, cart: @cart).call
+    redirect_back_with_flash(I18n.t('notice.book.added_to_cart'))
+  end
+
+  def update
+    UpdateCartItemService.new(id: params[:id], quantity: cart_item_params[:quantity]).call
+    redirect_back_with_flash(I18n.t('notice.book.count_changed'))
   end
 
   def destroy
     CartItem.find_by(id: params[:id]).destroy
-    redirect_to(cart_path, notice: I18n.t('notice.book.deleted'))
+    redirect_to(carts_path, notice: I18n.t('notice.book.deleted'))
   end
 
   private
 
+  def redirect_back_with_flash(message)
+    flash.notice = message
+    redirect_back fallback_location: root_path
+  end
+
   def cart_item_params
-    params.require(:cart_item).permit(:quantity, :book_id).merge(cart_id: @cart.id)
+    params.require(:cart_item).permit(:quantity, :book_id)
   end
 end
