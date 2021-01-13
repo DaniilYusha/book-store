e70ff74e784dfc06c1ba8c24d23531b70c9b19ac
DaniilYusha <ilushenko79@gmail.com>
2021-01-13 17:14:43 +0200

    Add changes to cart page

--- spec/pages/cart.rb
+++ spec/pages/cart.rb
@@ -1,6 +1,6 @@
 module Pages
   class Cart < SitePrism::Page
-    set_url '/carts/{id}'
+    set_url '/carts'
 
     section :orders, Sections::Cart::Orders, '.hidden-xs > table.table'
     section :order_summary, Sections::Cart::OrderSummary, '.general-order-wrap'
