4714b121d9da6c1b8a6b5cf0579c7ae319645c5f
DaniilYusha <ilushenko79@gmail.com>
2021-01-15 16:07:13 +0200

    Rename PersistCartItemService to PersistCartService

--- app/services/persist_cart_service.rb
+++ app/services/persist_cart_service.rb
@@ -1,17 +1,20 @@
-class PersistCartItemService
+class PersistCartService
+  attr_reader :cart
+
   def initialize(params:, cart:)
     @params = params
     @cart = cart
   end
 
   def call
+    @cart = Cart.create unless @cart
     item = find_item
     item ? update_item(item) : create_item
   end
 
   private
 
-  attr_reader :params, :cart
+  attr_reader :params
 
   def find_item
     cart.cart_items.find_by(book_id: params[:book_id])
