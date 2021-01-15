65380c454619d429946022245f8aaddae11d2c64
DaniilYusha <ilushenko79@gmail.com>
2021-01-15 17:26:10 +0200

    Add buy_first_book method to slider section in home page

--- spec/sections/slider.rb
+++ spec/sections/slider.rb
@@ -1,5 +1,9 @@
 module Sections
   class Slider < SitePrism::Section
     elements :buy_now_buttons, 'div.general-align>a.btn-primary'
+
+    def buy_first_book
+      buy_now_buttons.first.click
+    end
   end
 end
