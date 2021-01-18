2e03a4d773f0292bc909a62eaacc83ce845f9afa
DaniilYusha <ilushenko79@gmail.com>
2021-01-18 14:22:08 +0200

    Remove comments from carrierwave config file for rspec

--- spec/config/carrierwave.rb
+++ spec/config/carrierwave.rb
@@ -1,6 +1,3 @@
 CarrierWave.configure do |config|
   config.root = Rails.root.join('spec/fixtures')
-  # config.cache_only  = true
-  # config.enable_processing = false
-  # config.base_path = "/"
 end
