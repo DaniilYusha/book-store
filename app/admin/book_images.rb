73593d035940b8b1c82632f52d3e36f18dad37b0
DaniilYusha <ilushenko79@gmail.com>
2021-01-18 13:19:28 +0200

    Add changes to image output in admin/book_images

--- app/admin/book_images.rb
+++ app/admin/book_images.rb
@@ -1,23 +1,20 @@
 ActiveAdmin.register BookImage do
   permit_params :image, :book_id
   includes :book
+  decorate_with BookImageDecorator
 
   index do
     selectable_column
     id_column
     column :book
-    column :image do |book_image|
-      image_tag(book_image.image_url(:w170))
-    end
+    column :image
     actions
   end
 
   show do
     attributes_table do
       row :book
-      row :image do |book_image|
-        image_tag(book_image.image_url(:w170))
-      end
+      row :image
     end
   end
 end
