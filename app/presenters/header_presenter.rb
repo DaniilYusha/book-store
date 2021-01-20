709904990c8aba873d1da957217182601fcf9586
DaniilYusha <ilushenko79@gmail.com>
2021-01-20 14:23:16 +0200

    Add DEFAULT_BOOKS_COUNT constant to HeaderPresenter

--- app/presenters/header_presenter.rb
+++ app/presenters/header_presenter.rb
@@ -1,10 +1,12 @@
 class HeaderPresenter
+  DEFAULT_BOOKS_COUNT = 0
+
   def initialize(order:)
     @order = order
   end
 
   def books_in_order
-    order ? order.items_count : 0
+    order ? order.items_count : DEFAULT_BOOKS_COUNT
   end
 
   def categories
