65e4237edf1c6d7171ee7af536a53651172af9cc
DaniilYusha <ilushenko79@gmail.com>
2021-01-20 14:23:52 +0200

    Remove set_categories method from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_action :set_categories, :header_presenter, :current_order
+  before_action :header_presenter, :current_order
 
   private
-
-  def set_categories
-    @categories = Category.all
-  end
 
   def header_presenter
     @header_presenter = HeaderPresenter.new(order: current_order)
