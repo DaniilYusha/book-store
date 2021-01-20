0c938f2d9ea83c2c771e3f1a0268e868ddba87a0
DaniilYusha <ilushenko79@gmail.com>
2021-01-20 13:15:29 +0200

    Refactor DestroyOrderItemService

--- app/services/destroy_order_item_service.rb
+++ app/services/destroy_order_item_service.rb
@@ -1,12 +1,15 @@
 class DestroyOrderItemService
+  attr_reader :errors
+  
   def initialize(item_id:, order:)
     @id = item_id
     @order = order
+    @errors = []
   end
 
   def call
-    order.order_items.find_by(id: id).delete
-    order.destroy if order.order_items.blank?
+    delete_order_item
+    destroy_order
   end
 
   def order_destroyed?
@@ -16,4 +19,16 @@ class DestroyOrderItemService
   private
 
   attr_reader :id, :order
+
+  def delete_order_item
+    order_item ? order_item.delete : @errors << I18n.t('alert.something_wrong')
+  end
+
+  def destroy_order
+    order.destroy if order.order_items.empty?
+  end
+
+  def order_item
+    @order_item ||= order.order_items.find_by(id: id)
+  end
 end
