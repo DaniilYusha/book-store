90df375ef765ab22d7b915d075fea7aad71c2a3c
DaniilYusha <ilushenko79@gmail.com>
2021-01-22 17:08:15 +0200

    Add enum for status field in Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  enum status: { processing: 0, address: 1, delivery: 2, payment: 3, confirmation: 4, complete: 5 }
+
   has_many :order_items, dependent: :destroy
   has_one :coupon, dependent: :nullify
 
