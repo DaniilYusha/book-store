1075e3703327b61257fcbb7ebf1ef468fbc23f8c
DaniilYusha <ilushenko79@gmail.com>
2021-01-23 14:14:28 +0200

    Add specs for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,11 +1,20 @@
 RSpec.describe Order, type: :model do
+  subject(:order) { described_class.new }
+
   describe 'associations' do
     it { is_expected.to belong_to(:user).optional }
-    it { is_expected.to have_many(:cart_items).dependent(:destroy) }
+    it { is_expected.to have_many(:order_items).dependent(:destroy) }
     it { is_expected.to have_one(:coupon).dependent(:nullify) }
   end
 
   describe 'with database indexes' do
     it { is_expected.to have_db_index(:user_id) }
+  end
+
+  describe 'enum for status' do
+    it {
+      expect(order).to define_enum_for(:status)
+        .with_values(processing: 0, address: 1, delivery: 2, payment: 3, confirmation: 4, complete: 5)
+    }
   end
 end
