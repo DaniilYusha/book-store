0f28a77dfcbe92b09622ccd2dc829901f353c2d4
DaniilYusha <ilushenko79@gmail.com>
2021-01-23 14:05:59 +0200

    Refactor specs for OrderItemDecorator

--- spec/decorators/order_item_decorator_spec.rb
+++ spec/decorators/order_item_decorator_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe OrderItemDecorator do
-  let(:book) { build(:book, price: 5.00) }
-  let(:cart_item) { build(:cart_item, book: book, quantity: 2).decorate }
+  let(:order_item) { build(:order_item).decorate }
 
   describe '#subtotal_price' do
-    it { expect(cart_item.subtotal_price).to eq 10.00 }
+    let(:result) { order_item.quantity * order_item.book.price }
+
+    it { expect(order_item.subtotal_price).to eq(result) }
   end
 end
