39814d5ec515ecca3248da2c25b872283e593b18
DaniilYusha <ilushenko79@gmail.com>
2021-01-23 17:51:43 +0200

    Make seeds available only for development environment

--- db/seeds/books.seeds.rb
+++ db/seeds/books.seeds.rb
@@ -1,19 +1,19 @@
-# if Rails.env.development?
-after :authors do
-  authors = Author.all
-  25.times do
-    Book.create(
-      title: FFaker::Book.title,
-      description: FFaker::Book.description,
-      price: rand(1.0..1000.0).floor(2),
-      height: rand(7.0..40.0).floor(2),
-      width: rand(1.0..20.0).floor(2),
-      depth: rand(1.0..7.0).floor(2),
-      published_at: FFaker::Time.date,
-      materials: FFaker::Lorem.word,
-      category_id: rand(1..Category.count),
-      authors: [authors.sample, authors.sample]
-    )
+if Rails.env.development?
+  after :authors do
+    authors = Author.all
+    25.times do
+      Book.create(
+        title: FFaker::Book.title,
+        description: FFaker::Book.description,
+        price: rand(1.0..1000.0).floor(2),
+        height: rand(7.0..40.0).floor(2),
+        width: rand(1.0..20.0).floor(2),
+        depth: rand(1.0..7.0).floor(2),
+        published_at: FFaker::Time.date,
+        materials: FFaker::Lorem.word,
+        category_id: rand(1..Category.count),
+        authors: [authors.sample, authors.sample]
+      )
+    end
   end
 end
-# end
