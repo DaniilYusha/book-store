e4afe13e148fc65278d5faf81827e92f6ceb7659
DaniilYusha <ilushenko79@gmail.com>
2021-01-23 14:10:41 +0200

    Add changes to models/order_item_spec

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe OrderItem, type: :model do
   end
 
   describe 'associations' do
-    it { is_expected.to belong_to(:cart) }
+    it { is_expected.to belong_to(:order) }
     it { is_expected.to belong_to(:book) }
   end
 
   describe 'with database indexes' do
-    it { is_expected.to have_db_index(:cart_id) }
+    it { is_expected.to have_db_index(:order_id) }
     it { is_expected.to have_db_index(:book_id) }
   end
 end
