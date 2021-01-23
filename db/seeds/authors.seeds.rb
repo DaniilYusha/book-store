e6e8edab9dbc21c79f2468c55140b5b611d8c821
DaniilYusha <ilushenko79@gmail.com>
2021-01-23 17:30:49 +0200

    Make seeds available for each environment

--- db/seeds/authors.seeds.rb
+++ db/seeds/authors.seeds.rb
@@ -1,10 +1,10 @@
-if Rails.env.development?
-  after :categories do
-    8.times do
-      Author.create(
-        first_name: FFaker::Name.first_name,
-        last_name: FFaker::Name.last_name
-      )
-    end
+# if Rails.env.development?
+after :categories do
+  8.times do
+    Author.create(
+      first_name: FFaker::Name.first_name,
+      last_name: FFaker::Name.last_name
+    )
   end
 end
+# end
