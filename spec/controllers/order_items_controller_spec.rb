03ccab8e29ba9fe8dd61d3b5cbb758bf49cb8910
DaniilYusha <ilushenko79@gmail.com>
2021-01-23 13:30:15 +0200

    Fix specs for OrderItemsController

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -1,56 +1,89 @@
 RSpec.describe OrderItemsController, type: :controller do
   let(:book) { create(:book) }
 
-  describe 'POST /cart_items' do
-    before { post :create, params: { cart_item: params } }
+  describe 'POST /order_items' do
+    before { post :create, params: { order_item: params } }
 
-    context 'when create new cart item' do
-      let(:params) { attributes_for(:cart_item, book_id: book.id) }
+    context 'when create new order item' do
+      let(:params) { attributes_for(:order_item, book_id: book.id) }
 
       it { expect(response).to have_http_status(:redirect) }
-      it { expect(flash[:notice]).to eq(I18n.t('notice.book.added_to_cart')) }
-      it { expect(cookies[:cart_id].nil?).to be false }
+      it { expect(flash[:notice]).to eq(I18n.t('notice.book.added_to_order')) }
+      it { expect(cookies[:order_id].nil?).to be false }
     end
 
-    context 'when update existing cart item' do
-      let(:cart) { create(:cart) }
-      let(:params) { attributes_for(:cart_item, cart_id: cart.id, book_id: book.id) }
-      let(:cart_item) { create(:cart_item, cart_id: cart.id, book_id: book.id) }
+    context 'when update existing order item' do
+      let(:order) { create(:order) }
+      let(:params) { attributes_for(:order_item, order_id: order.id, book_id: book.id) }
+      let(:order_item) { create(:order_item, order_id: order.id, book_id: book.id) }
 
-      before { cookies[:cart_id] = cart.id }
+      before { cookies[:order_id] = order.id }
 
       it { expect(response).to have_http_status(:redirect) }
-      it { expect(flash[:notice]).to eq(I18n.t('notice.book.added_to_cart')) }
-      it { expect(cookies[:cart_id]).to eq(cart.id) }
+      it { expect(flash[:notice]).to eq(I18n.t('notice.book.added_to_order')) }
+      it { expect(cookies[:order_id]).to eq(order.id) }
     end
   end
 
-  describe 'PATCH /cart_item/{id}' do
-    let(:cart) { create(:cart) }
-    let(:cart_item) { create(:cart_item) }
-    let(:params) { attributes_for(:cart_item, cart_id: cart.id) }
+  describe 'PATCH /order_item/{id}' do
+    let(:order) { create(:order) }
 
     before do
-      cookies[:cart_id] = cart.id
-      patch :update, params: { id: cart_item.id, cart_item: params }
+      cookies[:order_id] = order.id
+      patch :update, params: { id: order_item.id, order_item: params }
     end
 
-    it { expect(response).to have_http_status(:redirect) }
-    it { expect(flash[:notice]).to eq(I18n.t('notice.book.count_changed')) }
-    it { expect(cookies[:cart_id].to_i).to eq(cart.id) }
+    context 'when receive failed response' do
+      let(:order_item) { create(:order_item, order: order) }
+      let(:params) { { decrement: true } }
+
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect(flash[:alert]).to eq(I18n.t('alert.something_wrong')) }
+      it { expect(cookies[:order_id].to_i).to eq(order.id) }
+    end
+
+    context 'when increment item quantity' do
+      let(:order_item) { create(:order_item, order: order) }
+      let(:params) { { increment: true } }
+
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect(flash[:notice]).to eq(I18n.t('notice.book.count_changed')) }
+      it { expect(cookies[:order_id].to_i).to eq(order.id) }
+    end
+
+    context 'when decrement item quantity' do
+      let(:order_item) { create(:order_item, quantity: 2, order: order) }
+      let(:params) { { decrement: true } }
+
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect(flash[:notice]).to eq(I18n.t('notice.book.count_changed')) }
+      it { expect(cookies[:order_id].to_i).to eq(order.id) }
+    end
   end
 
-  describe 'DELETE /cart_item/{id}' do
-    let(:cart) { create(:cart) }
-    let(:cart_item) { create(:cart_item, cart_id: cart.id) }
+  describe 'DELETE /order_item/{id}' do
+    let(:order) { create(:order) }
+    let(:order_item) { create(:order_item, order_id: order.id) }
 
     before do
-      cookies[:cart_id] = cart.id
-      delete :destroy, params: { id: cart_item.id }
+      cookies[:order_id] = order.id
+      delete :destroy, params: { id: id }
     end
 
-    it { expect(response).to have_http_status(:redirect) }
-    it { expect(flash[:notice]).to eq(I18n.t('notice.book.deleted')) }
-    it { expect(cookies[:cart_id].to_i).to eq(cart.id) }
+    context 'when successfully destroy order item' do
+      let(:id) { order_item.id }
+
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect(flash[:notice]).to eq(I18n.t('notice.book.deleted')) }
+      it { expect(cookies[:order_id].to_i).to eq(order.id) }
+    end
+
+    context 'when receive failed response' do
+      let(:id) { order_item.id.next }
+
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect(flash[:alert]).to eq(I18n.t('alert.something_wrong')) }
+      it { expect(cookies[:order_id].to_i).to eq(order.id) }
+    end
   end
 end
