b2035c5a40ae142814e0071189283774c2375053
DaniilYusha <ilushenko79@gmail.com>
2021-01-25 17:11:09 +0200

    Change alert messages for create and update methods in OrderItemsController

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,15 +1,15 @@
 class OrderItemsController < ApplicationController
   def create
     service = PersistOrderItemService.new(params: order_item_params, order: current_order)
-    return redirect_back_with_flash(:alert, service.errors.to_sentence) unless service.call
+    return redirect_back_with_flash(:alert, service.errors.full_messages.to_sentence) unless service.call
 
     cookies[:order_id] = service.order.id unless current_order
     redirect_back_with_flash(:notice, I18n.t('notice.book.added_to_order'))
   end
 
   def update
-    service = UpdateOrderItemService.new(id: params[:id], params: order_item_params)
-    return redirect_back_with_flash(:alert, service.errors.to_sentence) unless service.call
+    service = PersistOrderItemService.new(params: order_item_params, order: current_order)
+    return redirect_back_with_flash(:alert, service.errors.full_messages.to_sentence) unless service.call
 
     redirect_back_with_flash(:notice, I18n.t('notice.book.count_changed'))
   end
@@ -30,6 +30,6 @@ class OrderItemsController < ApplicationController
   end
 
   def order_item_params
-    params.require(:order_item).permit(:quantity, :increment, :decrement, :book_id)
+    params.require(:order_item).permit(:quantity, :book_id)
   end
 end
