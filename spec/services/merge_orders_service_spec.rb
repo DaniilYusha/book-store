821a7b031aaa10bb20c143770ec8dfdf248177c9
DaniilYusha <ilushenko79@gmail.com>
2021-01-26 16:26:51 +0200

    Fix specs for merge_orders_service_spec.rb

--- spec/services/merge_orders_service_spec.rb
+++ spec/services/merge_orders_service_spec.rb
@@ -1,29 +1,38 @@
 RSpec.describe MergeOrdersService do
-  let(:user) { create(:user) }
-  let(:order_item) { create(:order_item, quantity: 2) }
-  let(:order) { create(:order, order_items: [order_item]) }
-
   describe '#call' do
-    context 'when user order without items' do
-      let(:user_order) { create(:order, user: user) }
+    let(:order_item) { create(:order_item) }
+    let(:guest_order) { create(:order, order_items: [order_item], user: nil) }
 
-      it 'create new order for user and returns an Array' do
-        expect(described_class.new(order: order, user_order: user_order).call.class).to eq(Array)
+    context 'when user order without order' do
+      let(:user) { create(:user) }
+      let(:expected_count) { 1 }
+
+      it 'returns an Orders collection with only one order' do
+        expect(described_class.new(guest_order_id: guest_order.id, user: user).call.count).to eq(expected_count)
       end
 
-      it 'returns array with OrderItems' do
-        described_class.new(order: order, user_order: user_order).call.each do |item|
-          expect(item.class).to eq(OrderItem)
+      it 'returns array with Order' do
+        described_class.new(guest_order_id: guest_order.id, user: user).call.each do |order|
+          expect(order.class).to eq(Order)
         end
       end
     end
 
-    context 'when user order has same item with different quantity' do
+    context 'when user order has same item' do
       let(:user_order_item) { create(:order_item, book: order_item.book) }
-      let(:user_order) { create(:order, user: user, order_items: [user_order_item]) }
+      let(:user_order) { create(:order, order_items: [user_order_item]) }
+
+      it 'returns Order' do
+        expect(described_class.new(guest_order_id: guest_order.id, user: user_order.user).call.class).to eq(Order)
+      end
+    end
+
+    context 'when user order has different item' do
+      let(:user_order_item) { create(:order_item) }
+      let(:user_order) { create(:order, order_items: [user_order_item]) }
 
       it 'updates item quantity and returns nil' do
-        expect(described_class.new(order: order, user_order: user_order).call).to eq(nil)
+        expect(described_class.new(guest_order_id: guest_order.id, user: user_order.user).call.class).to eq(Order)
       end
     end
   end
