d20c5c9a4600db4a9efb519ded76e40b7a128569
DaniilYusha <ilushenko79@gmail.com>
2021-01-26 14:56:42 +0200

    Move logic from current_cart method to merge_orders method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_action :header_presenter, :current_order
+  before_action :header_presenter, :merge_orders, :current_order
 
   rescue_from ActiveRecord::RecordNotFound do |_exception|
     flash[:alert] = I18n.t('alert.something_wrong')
@@ -14,21 +14,14 @@ class ApplicationController < ActionController::Base
     @header_presenter = HeaderPresenter.new(order: current_order&.decorate)
   end
 
-  def current_order
-    order = Order.find_by(id: cookies[:order_id])
-    return order unless user_signed_in?
+  def merge_orders
+    return unless user_signed_in?
 
-    set_order_to_user(order, current_user.orders.find_by(status: :pending)) if order
-    current_user.orders.find_by(status: :pending)
+    MergeOrdersService.new(guest_order_id: cookies[:order_id], user: current_user).call
+    cookies.delete(:order_id)
   end
 
-  def set_order_to_user(order, user_order)
-    if user_order
-      MergeOrdersService.new(guest_order: order, user_order: user_order).call
-      Order.destroy(cookies[:order_id])
-    else
-      current_user.orders << order
-    end
-    cookies.delete(:order_id)
+  def current_order
+    user_signed_in? ? current_user.orders.find_by(status: :pending) : Order.find_by(id: cookies[:order_id])
   end
 end
