a422b9006a60357d0323dfd3326fe090e228c995
DaniilYusha <ilushenko79@gmail.com>
2021-01-26 14:10:37 +0200

    Refactor MergeOrdersService

--- app/services/merge_orders_service.rb
+++ app/services/merge_orders_service.rb
@@ -1,39 +1,29 @@
 class MergeOrdersService
-  def initialize(order:, user_order:)
-    @order = order
+  def initialize(guest_order:, user_order:)
+    @guest_order = guest_order
     @user_order = user_order
   end
 
   def call
-    update_user_items(user_order.order_items, order.order_items) if orders_have_same_items?
-    create_user_items(user_order, order.order_items) if order_contains_new_items?
+    guest_order.order_items.each do |guest_item|
+      user_order_has_guest_item?(guest_item) ? update_user_item_quantity(guest_item) : update_guest_item(guest_item)
+    end
   end
 
   private
 
-  attr_reader :order, :user_order
+  attr_reader :guest_order, :user_order
 
-  def create_user_items(user_order, order_items)
-    order_items.each do |order_item|
-      unless user_order.order_items.any? { |user_order_item| user_order_item.book_id == order_item.book_id }
-        user_order.order_items << order_item
-      end
-    end
+  def update_user_item_quantity(guest_item)
+    user_order_item = user_order.order_items.find_by(book_id: guest_item.book_id)
+    user_order_item.update(quantity: user_order_item.quantity + guest_item.quantity)
   end
 
-  def update_user_items(user_order_items, order_items)
-    order_items.each do |order_item|
-      user_order_items.where(book_id: order_item.book_id).each do |user_order_item|
-        user_order_item.update(quantity: user_order_item.quantity + order_item.quantity)
-      end
-    end
+  def update_guest_item(guest_item)
+    guest_item.update(order_id: user_order.id)
   end
 
-  def orders_have_same_items?
-    (user_order.order_items.map(&:book_id) & order.order_items.map(&:book_id)).any?
-  end
-
-  def order_contains_new_items?
-    (order.order_items.map(&:book_id) - user_order.order_items.map(&:book_id)).any?
+  def user_order_has_guest_item?(guest_item)
+    user_order.order_items.pluck(:book_id).include?(guest_item.book_id)
   end
 end
