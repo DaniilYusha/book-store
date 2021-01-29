41da5d13c49ea224d562fe2d48696ff56c97e25a
DaniilYusha <ilushenko79@gmail.com>
2021-01-29 17:37:18 +0200

    Add show and update methods to CheckoutController

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,3 +1,17 @@
 class CheckoutController < ApplicationController
-  def index; end
+  def show
+    service = CheckoutShowService.new(params: params, user: current_user, order: current_order)
+
+    @presenter = service.call
+    @order = current_order.decorate
+  end
+
+  def update
+    service = CheckoutUpdateService.new(params: params.permit!, user: current_user, order: current_order)
+    return redirect_to checkout_path(step: service.next_step) if service.call
+
+    @presenter = service.presenter
+    @order = current_order.decorate
+    render :show
+  end
 end
