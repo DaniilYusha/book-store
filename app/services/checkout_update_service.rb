f06276dece60ebfe1c2ac43781623b990869457a
DaniilYusha <ilushenko79@gmail.com>
2021-01-29 17:42:59 +0200

    Create CheckoutUpdateService

---
+++ app/services/checkout_update_service.rb
@@ -0,0 +1,37 @@
+class CheckoutUpdateService
+  attr_reader :presenter
+
+  SERVICES = {
+    address: AddressCheckoutService
+  }
+
+  STEPS = {
+    address: ->(order) { order.delivery! },
+    delivery: ->(order) { order.payment! },
+    payment: ->(order) { order.confirm! },
+    confirm: ->(order) { order.complete! }
+  }
+
+  def initialize(params:, user:, order:)
+    @params = params
+    @user = user
+    @order = order
+  end
+
+  def call
+    service = SERVICES[params[:step].to_sym].new(params: params, user: user)
+    return true if service.call
+
+    @presenter = service.presenter
+    false
+  end
+
+  def next_step
+    STEPS[params[:step].to_sym].call(order)
+    order.status
+  end
+
+  private
+
+  attr_reader :params, :user, :order
+end
