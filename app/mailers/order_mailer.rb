4c82aada21319da247747937f18d2ec91c5cf884
DaniilYusha <ilushenko79@gmail.com>
2021-02-08 17:07:25 +0200

    Add MAIL_FROM constant to OrderMailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,7 @@
 class OrderMailer < ApplicationMailer
-  default from: 'bookstore@example.com'
+  MAIL_FROM = 'bookstore@example.com'.freeze
+
+  default from: MAIL_FROM
 
   def thank_for_order(email)
     mail(to: email, body: I18n.t('checkout.confirm.thank_for_order'))
