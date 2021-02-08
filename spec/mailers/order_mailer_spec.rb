1275e9e9ec5998e410c0dfaeddb9a9b0c0df3413
DaniilYusha <ilushenko79@gmail.com>
2021-02-08 17:07:43 +0200

    Add specs for OrderMailer

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
-
 RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.thank_for_order' do
+    let(:email) { FFaker::Internet.free_email }
+    let(:mail) { described_class.thank_for_order(email) }
+
+    it 'has email recipient' do
+      expect(mail.to).to eq([email])
+    end
+
+    it 'has email sender' do
+      expect(mail.from).to eq([described_class::MAIL_FROM])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match(I18n.t('checkout.confirm.thank_for_order'))
+    end
+  end
 end
