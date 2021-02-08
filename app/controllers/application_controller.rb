6555f8688540e5f290fc5b09f29b311cb4159b63
DaniilYusha <ilushenko79@gmail.com>
2021-02-08 16:26:31 +0200

    Add changes to current_order method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   def current_order
     return Order.includes(order_items: [:book]).find_by(id: cookies[:order_id]) unless user_signed_in?
 
-    Order.includes(order_items: [:book]).where('status < ?', Order.statuses[:complete]).find_by(user_id: current_user.id)
+    Order.includes(order_items: [:book]).where('status < ?', Order.statuses[:complete]).find_by(user: current_user)
   end
 
   def redirect_back_with_flash(flash_type, message)
