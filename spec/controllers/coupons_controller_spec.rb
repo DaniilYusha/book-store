161bcec04af5e144803c2a5bc98225bc6b888c54
DaniilYusha <ilushenko79@gmail.com>
2021-02-08 13:04:31 +0200

    Add description for its in coupons_controller_spec

--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -8,8 +8,13 @@ RSpec.describe CouponsController, type: :controller do
     context 'with valid params' do
       let(:params) { { coupon: { code: coupon.code } } }
 
-      it { expect(response).to have_http_status(:redirect) }
-      it { expect(flash[:notice]).to eq(I18n.t('notice.coupon.apply')) }
+      it 'has redirect http status' do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'has notice flash' do
+        expect(flash[:notice]).to eq(I18n.t('notice.coupon.apply'))
+      end
     end
 
     context 'with invalid params' do
@@ -17,8 +22,13 @@ RSpec.describe CouponsController, type: :controller do
       let(:params) { { coupon: { code: fake_code } } }
       let(:error) { "Code #{I18n.t('alert.coupon.not_exist')}" }
 
-      it { expect(response).to have_http_status(:redirect) }
-      it { expect(flash[:alert]).to eq(error) }
+      it 'has redirect http status' do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'has alert flash' do
+        expect(flash[:alert]).to eq(error)
+      end
     end
   end
 end
