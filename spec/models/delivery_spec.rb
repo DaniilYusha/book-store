d4a0537f358cbb4119e8c2d4815388c7544748b1
DaniilYusha <ilushenko79@gmail.com>
2021-02-08 16:52:23 +0200

    Add specs for Delivery model

--- spec/models/delivery_spec.rb
+++ spec/models/delivery_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
-
 RSpec.describe Delivery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'with database columns' do
+    it { is_expected.to have_db_column(:method).of_type(:string) }
+    it { is_expected.to have_db_column(:price).of_type(:decimal) }
+    it { is_expected.to have_db_column(:from_days).of_type(:integer) }
+    it { is_expected.to have_db_column(:to_days).of_type(:integer) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:orders).dependent(:destroy) }
+  end
 end
