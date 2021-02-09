83e6689af2a2f365a4cc70be1c33cd57e86edd48
DaniilYusha <ilushenko79@gmail.com>
2021-02-09 12:27:31 +0200

    Add fields for deliveries factory

--- spec/factories/deliveries.rb
+++ spec/factories/deliveries.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :delivery do
+    method { FFaker::BaconIpsum.word }
+    price { rand(1.0..1000.0).floor(2) }
+    from_days { rand(1..10) }
+    to_days { rand(11..20) }
   end
 end
