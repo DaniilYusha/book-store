e94b5d73303867bb7725268adaf3d9cedb2a87cd
DaniilYusha <ilushenko79@gmail.com>
2021-02-09 12:34:41 +0200

    Remove method field from deliveries factory

--- spec/factories/deliveries.rb
+++ spec/factories/deliveries.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :delivery do
-    method { FFaker::BaconIpsum.word }
     price { rand(1.0..1000.0).floor(2) }
     from_days { rand(1..10) }
     to_days { rand(11..20) }
