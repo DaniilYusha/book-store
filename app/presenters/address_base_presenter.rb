6e7dfb234adc38c1a6b1259a688d4af4f94ffffe
DaniilYusha <ilushenko79@gmail.com>
2021-02-13 15:10:33 +0200

    Remove billing_address and shipping_address methods from AddressBasePresenter

--- app/presenters/address_base_presenter.rb
+++ app/presenters/address_base_presenter.rb
@@ -4,14 +4,6 @@ class AddressBasePresenter
     @params = params
   end
 
-  def billing_address
-    user.billing_address || Address.new
-  end
-
-  def shipping_address
-    user.shipping_address || Address.new
-  end
-
   def countries
     @countries ||= ISO3166::Country.all.sort_by(&:name)
   end
