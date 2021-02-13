db213525e34744b433a01bba6bf562179938e1f6
DaniilYusha <ilushenko79@gmail.com>
2021-02-13 16:40:56 +0200

    Create BestBooksService

---
+++ app/services/best_books_service.rb
@@ -0,0 +1,21 @@
+class BestBooksService
+  BEST_BOOKS_COUNT = 4
+
+  def self.call
+    new.call
+  end
+
+  def call
+    Book.includes(:authors).where(id: books_ids)
+  end
+
+  private
+
+  def books_ids
+    books_ids_with_count.map(&:first)
+  end
+
+  def books_ids_with_count
+    Book.joins(:order_items).group(:id).count.sort_by { |_key, value| -value }.first(BEST_BOOKS_COUNT)
+  end
+end
