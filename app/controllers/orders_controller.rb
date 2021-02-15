5645242aaf40f5e26e60edd2b4c28b9c6dc9a461
DaniilYusha <ilushenko79@gmail.com>
2021-02-15 14:56:30 +0200

    Add OrdersController with index method

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,12 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = SortedOrdersQuery.new(relation: current_user.orders, sort_by: orders_params[:sort_by]).call.decorate
+    @presenter = OrdersPresenter.new
+  end
+
+  private
+
+  def orders_params
+    params.permit(:sort_by)
+  end
+end
