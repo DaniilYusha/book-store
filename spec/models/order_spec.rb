f81946b5cea51d0e35d8c95c411c3c47e6099613
DaniilYusha <ilushenko79@gmail.com>
2021-02-16 12:22:21 +0200

    Add specs for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -15,9 +15,10 @@ RSpec.describe Order, type: :model do
   end
 
   describe 'enum for status' do
-    it {
-      expect(order).to define_enum_for(:status)
-        .with_values(pending: 0, address: 1, delivery: 2, payment: 3, confirm: 4, complete: 5)
-    }
+    it do
+      expect(order).to define_enum_for(:status).with_values(
+        pending: 0, address: 1, delivery: 2, payment: 3, confirm: 4,
+        complete: 5, in_delivery: 6, delivered: 7, canceled: 8)
+    end
   end
 end
